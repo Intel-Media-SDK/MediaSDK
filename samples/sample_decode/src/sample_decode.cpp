6a65aa9307cfe651425fa02ca31bc41618abdb07
Andrey Larionov <andrey.larionov@intel.com>
2018-02-05 18:25:55 +0300

    [samples][sample_decode] re-store "-DecodedOrder" option in CLI

--- samples/sample_decode/src/sample_decode.cpp
+++ samples/sample_decode/src/sample_decode.cpp
@@ -103,7 +103,7 @@ void PrintHelp(msdk_char *strAppName, const msdk_char *strErrorMessage)
     msdk_printf(MSDK_STRING("   [-async]                  - depth of asynchronous pipeline. default value is 4. must be between 1 and 20\n"));
     msdk_printf(MSDK_STRING("   [-gpucopy::<on,off>] Enable or disable GPU copy mode\n"));
     msdk_printf(MSDK_STRING("   [-timeout]                - timeout in seconds\n"));
-    msdk_printf(MSDK_STRING("   [-DecodedOrder]           - disable frame reordering in decoder\n"));
+    msdk_printf(MSDK_STRING("   [-decoded_order]          - disable frame reordering in decoder\n"));
 #if MFX_VERSION >= 1022
     msdk_printf(MSDK_STRING("   [-dec_postproc force/auto] - resize after decoder using direct pipe\n"));
     msdk_printf(MSDK_STRING("                  force: instruct to use decoder-based post processing\n"));
@@ -359,7 +359,7 @@ mfxStatus ParseInputString(msdk_char* strInput[], mfxU8 nArgNum, sInputParams* p
                 return MFX_ERR_UNSUPPORTED;
             }
         }
-        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-DecodedOrder")))
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-decoded_order")))
         {
             pParams->DecodedOrder = true;
         }
