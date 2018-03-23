992dffc2f6b2d9d97d5606dd98a7e4c9ea0a6482
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-03-23 17:08:50 +0300

    Fix build issue on Android after API update
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- _studio/mfx_lib/Android.mk
+++ _studio/mfx_lib/Android.mk
@@ -40,7 +40,8 @@ MFX_LOCAL_C_INCLUDES_HW := \
     $(MFX_LOCAL_C_INCLUDES_IMPL) \
     $(foreach dir, $(MFX_LOCAL_DIRS_HW), $(wildcard $(LOCAL_PATH)/mfx_lib/$(dir)/include)) \
     $(MFX_HOME)/_studio/mfx_lib/genx/field_copy/include \
-    $(MFX_HOME)/_studio/mfx_lib/genx/copy_kernels/include
+    $(MFX_HOME)/_studio/mfx_lib/genx/copy_kernels/include \
+    $(MFX_HOME)/_studio/shared/asc/include
 
 MFX_LOCAL_STATIC_LIBRARIES_HW := \
     libmfx_lib_merged_hw \
@@ -49,6 +50,7 @@ MFX_LOCAL_STATIC_LIBRARIES_HW := \
     libumc_io_merged_hw \
     libumc_core_merged_hw \
     libmfx_trace_hw \
+    libasc \
     libsafec
 
 MFX_LOCAL_LDFLAGS_HW += \
@@ -99,7 +101,8 @@ MFX_SHARED_FILES_HW += $(addprefix mfx_lib/genx/copy_kernels/src/, \
     genx_skl_copy_isa.cpp)
 
 MFX_SHARED_FILES_HW += $(addprefix mfx_lib/genx/field_copy/src/, \
-    genx_fcopy_cmcode_isa.cpp)
+    genx_fcopy_gen8_isa.cpp \
+    genx_fcopy_gen9_isa.cpp)
 
 MFX_LIB_SHARED_FILES_1 := $(addprefix mfx_lib/shared/src/, \
     libmfxsw.cpp \
