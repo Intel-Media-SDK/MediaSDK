d4ff32931a1886abc576608710039bc3227c1188
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-04-05 19:19:02 +0300

    Enable VP9 decoder for Android
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- _studio/mfx_lib/Android.mk
+++ _studio/mfx_lib/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(MFX_HOME)/_studio
 
 # =============================================================================
 
-MFX_LOCAL_DECODERS := h265 h264 mpeg2 vc1 mjpeg vp8
+MFX_LOCAL_DECODERS := h265 h264 mpeg2 vc1 mjpeg vp8 vp9
 MFX_LOCAL_ENCODERS := h264 mjpeg
 
 # Setting subdirectories to march thru
@@ -64,7 +64,7 @@ UMC_DIRS := \
     brc
 
 UMC_DIRS_IMPL := \
-    h265_dec h264_dec mpeg2_dec vc1_dec jpeg_dec \
+    h265_dec h264_dec mpeg2_dec vc1_dec jpeg_dec vp9_dec \
     vc1_common jpeg_common
 
 UMC_LOCAL_C_INCLUDES := \
