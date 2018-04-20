7947d2c72b6ca471d863baf57a1217a4149dc500
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-04-20 03:30:29 -0700

    Remove BXT platform limitations from some AVC/HEVC ROI features
    Issue: MDP-41097
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/hevce_hw/h265/src/mfx_h265_encode_vaapi.cpp
+++ _studio/hevce_hw/h265/src/mfx_h265_encode_vaapi.cpp
@@ -93,14 +93,12 @@ static mfxStatus SetROI(
         roi_Param->max_delta_qp = 51;
         roi_Param->min_delta_qp = -51;
 
-#if defined(LINUX_TARGET_PLATFORM_BXTMIN) || defined(LINUX_TARGET_PLATFORM_BXT)
         roi_Param->roi_flags.bits.roi_value_is_qp_delta = 0;
 #if MFX_VERSION > 1021
         if (task.m_roiMode == MFX_ROI_MODE_QP_DELTA) {
             roi_Param->roi_flags.bits.roi_value_is_qp_delta = 1;
         }
 #endif // MFX_VERSION > 1021
-#endif // defined(LINUX_TARGET_PLATFORM_BXTMIN) || defined(LINUX_TARGET_PLATFORM_BXT)
     }
 
     vaSts = vaUnmapBuffer(vaDisplay, roiParam_id);
