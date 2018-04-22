d01064a995a1710c2a29c7a2bae5472d39e6696a
Dmitry Ermilov <dmitry.ermilov@intel.com>
2018-04-22 10:42:49 -0400

    [vp8] macroblock_offset alignment
    The change depends on a corresponding driver fix.
    
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- _studio/mfx_lib/decode/vp8/src/mfx_vp8_dec_decode_hw.cpp
+++ _studio/mfx_lib/decode/vp8/src/mfx_vp8_dec_decode_hw.cpp
@@ -1374,12 +1374,10 @@ mfxStatus VideoDECODEVP8_HW::DecodeFrameHeader(mfxBitstream *in)
         while (++i < 2);
     }
 
-    m_frame_info.entropyDecSize = m_boolDecoder[VP8_FIRST_PARTITION].pos() * 8 - 16 - m_boolDecoder[VP8_FIRST_PARTITION].bitcount();
+    m_frame_info.entropyDecSize = m_boolDecoder[VP8_FIRST_PARTITION].pos() * 8 - 3*8 - m_boolDecoder[VP8_FIRST_PARTITION].bitcount();
 
-
-    int fix = (m_boolDecoder[VP8_FIRST_PARTITION].bitcount() & 0x7) ? 1 : 0;
-    m_frame_info.firstPartitionSize = m_frame_info.firstPartitionSize - (m_boolDecoder[VP8_FIRST_PARTITION].pos() - 3 + fix);
-
+    mfxU32 remaining_bits = m_boolDecoder[VP8_FIRST_PARTITION].bitcount() & 0x7;
+    m_frame_info.firstPartitionSize = first_partition_size - (m_boolDecoder[VP8_FIRST_PARTITION].pos() - 3 + (remaining_bits ? 1 : 0) );
 
     return MFX_ERR_NONE;
 }
