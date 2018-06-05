e0ca0bdb9bb6bad1cabb75a27a6af98bd546cfb3
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2018-06-05 19:23:21 +0300

    [HEVC FEI] Fixed HW encoder class creation

--- _studio/hevc_fei/h265_fei/include/mfx_h265_fei_encode_hw.h
+++ _studio/hevc_fei/h265_fei/include/mfx_h265_fei_encode_hw.h
@@ -37,10 +37,20 @@ public:
     H265FeiEncode_HW(mfxCoreInterface *core, mfxStatus *status)
         :MFXVideoENCODEH265_HW(core, status)
     {}
+
     virtual ~H265FeiEncode_HW()
     {
         Close();
     }
+
+    virtual MfxHwH265Encode::DriverEncoder* CreateHWh265Encoder(MFXCoreInterface* core, MfxHwH265Encode::ENCODER_TYPE type = MfxHwH265Encode::ENCODER_DEFAULT)
+    {
+        core;
+        type;
+
+        return new VAAPIh265FeiEncoder;
+    }
+
     virtual mfxStatus Reset(mfxVideoParam *par)
     {
         // waiting for submitted in driver tasks
@@ -114,7 +124,7 @@ public:
             return MFX_ERR_NOT_FOUND;
         }
 
-        H265FeiEncodePlugin* tmp_pplg = 0;
+        H265FeiEncodePlugin* tmp_pplg = nullptr;
 
         try
         {
@@ -126,7 +136,6 @@ public:
         }
         catch(...)
         {
-            delete tmp_pplg;
             return MFX_ERR_UNKNOWN;
         }
 
@@ -223,7 +232,13 @@ protected:
     explicit H265FeiEncodePlugin(bool CreateByDispatcher);
     virtual ~H265FeiEncodePlugin();
 
-    virtual MfxHwH265Encode::DriverEncoder* CreateHWh265Encoder(MFXCoreInterface* core, MfxHwH265Encode::ENCODER_TYPE type = MfxHwH265Encode::ENCODER_DEFAULT);
+    virtual MfxHwH265Encode::DriverEncoder* CreateHWh265Encoder(MFXCoreInterface* core, MfxHwH265Encode::ENCODER_TYPE type = MfxHwH265Encode::ENCODER_DEFAULT)
+    {
+        if (m_pImpl.get())
+            return m_pImpl->CreateHWh265Encoder(core, type);
+        else
+            return nullptr;
+    }
 
     virtual mfxStatus ExtraParametersCheck(mfxEncodeCtrl *ctrl, mfxFrameSurface1 *surface, mfxBitstream *bs)
     {
