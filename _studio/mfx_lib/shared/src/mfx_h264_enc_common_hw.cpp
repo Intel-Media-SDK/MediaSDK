980a42e4fd9da427256a44b1f0e1a0892f29b805
Artem Shaporenko <artem.shaporenko@intel.com>
2018-06-19 15:11:06 +0300

    [mfe] fix wrn in case of IntraRefresh
    Fixed warning reporting in case MaxNumFrames = 0 and auto mode.
    
    Signed-off-by: Artem Shaporenko <artem.shaporenko@intel.com>

--- _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -4658,7 +4658,7 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         mfeParam.MaxNumFrames = numFrames;
         changed = true;
     }
-    if (extOpt2->IntRefType && (mfeParam.MaxNumFrames > 1 || mfeParam.MFMode >= MFX_MF_AUTO))
+    if (extOpt2->IntRefType && (mfeParam.MaxNumFrames > 1 || (!mfeParam.MaxNumFrames && mfeParam.MFMode >= MFX_MF_AUTO)))
     {
         mfeParam.MaxNumFrames = 1;
         changed = true;
