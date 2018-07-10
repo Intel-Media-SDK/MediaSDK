49718aa32e18ab0be4f08257dc1b148c20c6e7b1
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-07-10 23:28:11 +0300

    [gcc-8.1] fix some warnings in hevc fei samples
    Fixes for #359
    
    This fixes:
     * ‘__builtin___snprintf_chk’ output may be truncated before the last format character [-Werror=format-truncation=]
     * _some_ memsets for non-trivial classes
    
    There are still few warnings left though in sample_hevc_fei_abr.
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- samples/sample_hevc_fei_abr/src/pipeline_hevc_fei.cpp
+++ samples/sample_hevc_fei_abr/src/pipeline_hevc_fei.cpp
@@ -230,9 +230,10 @@ void CFeiTranscodingPipeline::PrintInfo()
         const msdk_char* GopStrict = (gopOptFlag & MFX_GOP_STRICT) == 0 ? MSDK_STRING("Not Strict") : MSDK_STRING("Strict");
         msdk_printf(MSDK_STRING("\nGopOptFlag       :\t%s, %s"), GopClosed, GopStrict);
 
-        msdk_char idrInterval[6];
-        msdk_itoa_decimal(param.mfx.IdrInterval, idrInterval);
-        msdk_printf(MSDK_STRING("\nIDR Interval     :\t%s"), param.mfx.IdrInterval == 0xffff ? MSDK_STRING("Infinite") : MSDK_STRING(idrInterval));
+	if (param.mfx.IdrInterval == 0xffff)
+            msdk_printf(MSDK_STRING("\nIDR Interval     :\tInfinite"));
+	else
+	    msdk_printf(MSDK_STRING("\nIDR Interval     :\t%d"), param.mfx.IdrInterval);
 
         msdk_printf(MSDK_STRING("\nGopRefDist       :\t%d"), param.mfx.GopRefDist);
         msdk_printf(MSDK_STRING("\nNumRefFrame      :\t%d"), param.mfx.NumRefFrame);
