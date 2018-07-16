6f26423a3091b429365fbdae2dfb26ae93773ddb
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-07-16 06:36:39 -0700

    build: install include files to $prefix/include/mfx
    Fixes: #199
    
    This changes location of include files from $prefix/include to
    $prefix/include/mfx which is a usual way to install include files
    from the specific library on Linux. As of now ffmpeg-qsv awaits
    exactly this layout, thus we become compatible.
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- api/opensource/mfx_dispatch/pkg-config-shared.pc.cmake
+++ api/opensource/mfx_dispatch/pkg-config-shared.pc.cmake
@@ -6,5 +6,5 @@ prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@CMAKE_INSTALL_FULL_LIBDIR@
 includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
 Libs: -L${libdir} -ldispatch_shared -lstdc++ -ldl
-Cflags: -I${includedir} -DMFX_DISPATCHER_EXPOSED_PREFIX
+Cflags: -I${includedir} -I${includedir}/mfx -DMFX_DISPATCHER_EXPOSED_PREFIX
 
