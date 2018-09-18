1a7c12ea51b3c856d51a10eedea4711acd3abc1a
Igor Lapichev <igorx.lapichev@intel.com>
2018-09-18 07:52:28 +0300

    LINUX_TARGET_PLATFORM_BXT was removed from mfx_h265_encode_hw_par.cpp

--- _studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_par.cpp
+++ _studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -945,8 +945,6 @@ mfxStatus CheckAndFixRoi(MfxVideoParam  const & par, ENCODE_CAPS_HEVC const & ca
             invalid++;
     }
 
-    //// TODO: remove below macro conditional statement when ROI related caps will be correctly set up by the driver
-#if !defined(LINUX_TARGET_PLATFORM_BXTMIN) && !defined(LINUX_TARGET_PLATFORM_BXT) && !defined(LINUX_TARGET_PLATFORM_CFL)
     if (par.mfx.RateControlMethod == MFX_RATECONTROL_CQP) {
         invalid += (caps.ROIDeltaQPSupport == 0);
     }
@@ -967,7 +965,6 @@ mfxStatus CheckAndFixRoi(MfxVideoParam  const & par, ENCODE_CAPS_HEVC const & ca
             invalid++;
 #endif // MFX_VERSION > 1021
     }
-#endif  // LINUX_TARGET_PLATFORM_BXTMIN
 
     mfxU16 maxNumOfRoi = (caps.MaxNumOfROI <= MAX_NUM_ROI  && (!bROIViaMBQP)) ? caps.MaxNumOfROI : MAX_NUM_ROI;
 
