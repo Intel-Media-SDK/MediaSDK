edb36df94d78ec262f23fa977016293d8e67c363
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-09-19 07:42:25 -0700

    h264e: enable ICQ rate control
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -2409,6 +2409,7 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
        par.mfx.RateControlMethod != MFX_RATECONTROL_CBR &&
        par.mfx.RateControlMethod != MFX_RATECONTROL_VBR &&
        par.mfx.RateControlMethod != MFX_RATECONTROL_CQP &&
+       par.mfx.RateControlMethod != MFX_RATECONTROL_ICQ &&
        !bRateControlLA(par.mfx.RateControlMethod))
     {
         unsupported = true;
