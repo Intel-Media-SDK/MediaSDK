3e5a5c03b747a889551f6dd7130bcf8c5a1efc2e
Ilya Sergeyev <ilyax.sergeev@intel.com>
2018-10-24 12:16:06 +0300

    clang 6.0: warning: suggest braces around initialization of subobject

--- samples/sample_vpp/src/sample_vpp_utils.cpp
+++ samples/sample_vpp/src/sample_vpp_utils.cpp
@@ -401,7 +401,7 @@ mfxStatus InitParamsVPP(mfxVideoParam* pParams, sInputParams* pInParams, mfxU32 
 mfxStatus CreateFrameProcessor(sFrameProcessor* pProcessor, mfxVideoParam* pParams, sInputParams* pInParams)
 {
     mfxStatus  sts = MFX_ERR_NONE;
-    mfxVersion version = {10, 1};
+    mfxVersion version = { {10, 1} };
     mfxIMPL    impl    = pInParams->ImpLib;
 
     MSDK_CHECK_POINTER(pProcessor, MFX_ERR_NULL_PTR);
