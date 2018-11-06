Intel-Media-SDK/MediaSDK

commit 29d17bd53d5b04a0ba29b2bfe09c48e3faec9599

[sample_hevc_fei] Support setting some cli options per frame type
Add ForceCtuSplit, NumFramePartitions and FastIntraMode for setting
per frame type for better experience in quality/performance trade-off.
Also during experiments NumFramePartitions=16 showed incorrect behavior
so it's removed from supported values.

samples/sample_hevc_fei/src/sample_hevc_fei.cpp
@@ -144,10 +144,19 @@ void PrintHelp(const msdk_char *strAppName, const msdk_char *strErrorMessage)
 
     msdk_printf(MSDK_STRING("Partitioning: \n"));
     msdk_printf(MSDK_STRING("   [-ForceCtuSplit]          - force splitting CTU into CU at least once\n"));
-    msdk_printf(MSDK_STRING("   [-NumFramePartitions num] - number of partitions in frame that encoder processes concurrently (1, 2, 4, 8 or 16)\n"));
-    
+    msdk_printf(MSDK_STRING("   [-ForceCtuSplit:I]        - force splitting CTU into CU at least once on I-frames only\n"));
+    msdk_printf(MSDK_STRING("   [-ForceCtuSplit:P]        - force splitting CTU into CU at least once on P/GPB-frames only\n"));
+    msdk_printf(MSDK_STRING("   [-ForceCtuSplit:B]        - force splitting CTU into CU at least once on B-frames only\n"));
+    msdk_printf(MSDK_STRING("   [-NumFramePartitions num] - number of partitions in frame that encoder processes concurrently (1, 2, 4 or 8)\n"));
+    msdk_printf(MSDK_STRING("   [-NFP:I num]              - number of partitions in I frame that encoder processes concurrently (1, 2, 4 or 8)\n"));
+    msdk_printf(MSDK_STRING("   [-NFP:P num]              - number of partitions in P/GPB frame that encoder processes concurrently (1, 2, 4 or 8)\n"));
+    msdk_printf(MSDK_STRING("   [-NFP:B num]              - number of partitions in B frame that encoder processes concurrently (1, 2, 4 or 8)\n"));
+
     msdk_printf(MSDK_STRING("Force Flags: \n"));
     msdk_printf(MSDK_STRING("   [-FastIntraMode] - force encoder to skip HEVC-specific intra modes (use AVC modes only)\n"));
+    msdk_printf(MSDK_STRING("   [-FastIntra:I]   - force encoder to skip HEVC-specific intra modes (use AVC modes only) on I-frames\n"));
+    msdk_printf(MSDK_STRING("   [-FastIntra:P]   - force encoder to skip HEVC-specific intra modes (use AVC modes only) on P/GPB-frames\n"));
+    msdk_printf(MSDK_STRING("   [-FastIntra:B]   - force encoder to skip HEVC-specific intra modes (use AVC modes only) on B-frames\n"));
 
     msdk_printf(MSDK_STRING("Motion Search: \n"));
     msdk_printf(MSDK_STRING("   [-SearchWindow value]         - specifies one of the predefined search path and window size. In range [1,5] (5 is default).\n"));
@@ -415,16 +424,61 @@ mfxStatus ParseInputString(msdk_char* strInput[], mfxU32 nArgNum, sInputParams& 
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit")))
         {
-            params.encodeCtrl.ForceCtuSplit = 1;
+            params.frameCtrl.CtrlI.ForceCtuSplit = params.frameCtrl.CtrlP.ForceCtuSplit = params.frameCtrl.CtrlB.ForceCtuSplit = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit:I")))
+        {
+            params.frameCtrl.CtrlI.ForceCtuSplit = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit:P")))
+        {
+            params.frameCtrl.CtrlP.ForceCtuSplit = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit:B")))
+        {
+            params.frameCtrl.CtrlB.ForceCtuSplit = 1;
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NumFramePartitions")))
         {
             CHECK_NEXT_VAL(i + 1 >= nArgNum, strInput[i], strInput[0]);
-            PARSE_CHECK(msdk_opt_read(strInput[++i], params.encodeCtrl.NumFramePartitions), "NumFramePartitions", isParseInvalid)
+            PARSE_CHECK(msdk_opt_read(strInput[++i], params.encodeCtrl.NumFramePartitions), "NumFramePartitions", isParseInvalid);
+
+            params.frameCtrl.CtrlI.NumFramePartitions =
+                params.frameCtrl.CtrlP.NumFramePartitions =
+                params.frameCtrl.CtrlB.NumFramePartitions = params.encodeCtrl.NumFramePartitions;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NFP:I")))
+        {
+            CHECK_NEXT_VAL(i + 1 >= nArgNum, strInput[i], strInput[0]);
+            PARSE_CHECK(msdk_opt_read(strInput[++i], params.frameCtrl.CtrlI.NumFramePartitions), "NumFramePartitions", isParseInvalid);
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NFP:P")))
+        {
+            CHECK_NEXT_VAL(i + 1 >= nArgNum, strInput[i], strInput[0]);
+            PARSE_CHECK(msdk_opt_read(strInput[++i], params.frameCtrl.CtrlP.NumFramePartitions), "NumFramePartitions", isParseInvalid);
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NFP:B")))
+        {
+            CHECK_NEXT_VAL(i + 1 >= nArgNum, strInput[i], strInput[0]);
+            PARSE_CHECK(msdk_opt_read(strInput[++i], params.frameCtrl.CtrlB.NumFramePartitions), "NumFramePartitions", isParseInvalid);
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntraMode")))
         {
-            params.encodeCtrl.FastIntraMode = 1;
+            params.frameCtrl.CtrlI.FastIntraMode =
+                params.frameCtrl.CtrlP.FastIntraMode =
+                params.frameCtrl.CtrlB.FastIntraMode = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntra:I")))
+        {
+            params.frameCtrl.CtrlI.FastIntraMode = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntra:P")))
+        {
+            params.frameCtrl.CtrlP.FastIntraMode = 1;
+        }
+        else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntra:B")))
+        {
+            params.frameCtrl.CtrlB.FastIntraMode = 1;
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-tff")))
         {
@@ -596,13 +650,20 @@ mfxStatus CheckOptions(const sInputParams& params, const msdk_char* appName)
         PrintHelp(appName, "Unsupported NumMvPredictorsL1 value (must be in range [0,4])");
         return MFX_ERR_UNSUPPORTED;
     }
-    if (!(params.encodeCtrl.NumFramePartitions == 1 ||
-          params.encodeCtrl.NumFramePartitions == 2 ||
-          params.encodeCtrl.NumFramePartitions == 4 ||
-          params.encodeCtrl.NumFramePartitions == 8 ||
-          params.encodeCtrl.NumFramePartitions == 16  ))
+    std::list<mfxU16> nfp_values = {1, 2, 4, 8};
+    if (std::find(nfp_values.begin(), nfp_values.end(), params.frameCtrl.CtrlI.NumFramePartitions) == nfp_values.end())
     {
-        PrintHelp(appName, "Unsupported NumFramePartitions value (must be 1, 2, 4, 8 or 16)");
+        PrintHelp(appName, "Unsupported NumFramePartitions value for I frames (must be 1, 2, 4 or 8)");
+        return MFX_ERR_UNSUPPORTED;
+    }
+    if (std::find(nfp_values.begin(), nfp_values.end(), params.frameCtrl.CtrlP.NumFramePartitions) == nfp_values.end())
+    {
+        PrintHelp(appName, "Unsupported NumFramePartitions value for P/GPB frames (must be 1, 2, 4 or 8)");
+        return MFX_ERR_UNSUPPORTED;
+    }
+    if (std::find(nfp_values.begin(), nfp_values.end(), params.frameCtrl.CtrlB.NumFramePartitions) == nfp_values.end())
+    {
+        PrintHelp(appName, "Unsupported NumFramePartitions value for B frames (must be 1, 2, 4 or 8)");
         return MFX_ERR_UNSUPPORTED;
     }
     if (!params.bENCODE && !params.bPREENC)

samples/sample_hevc_fei/include/hevc_fei_encode.h
@@ -31,7 +31,8 @@ class FEI_Encode
 {
 public:
     FEI_Encode(MFXVideoSession* session, mfxHDL hdl,
-        MfxVideoParamsWrapper& encode_pars, const mfxExtFeiHevcEncFrameCtrl& encodeCtrl,
+        MfxVideoParamsWrapper& encode_pars,
+        const mfxExtFeiHevcEncFrameCtrl& frameCtrl, const PerFrameTypeCtrl& frametypeCtrl,
         const msdk_char* strDstFile, const msdk_char* mvpInFile,
         const msdk_char* repackctrlFile, const msdk_char* repackstatFile,
         PredictorsRepaking* repacker);
@@ -72,7 +73,8 @@ private:
 
     std::auto_ptr<PredictorsRepaking> m_repacker;
 
-    mfxExtFeiHevcEncFrameCtrl m_defFrameCtrl; // contain default per-frame options including user-specified
+    mfxExtFeiHevcEncFrameCtrl m_defFrameCtrl;     // contain default and user-specified options per frame
+    PerFrameTypeCtrl          m_ctrlPerFrameType; // contain default and user-specified options per frame type
 
     mfxU32  m_processedFrames;
 

samples/sample_hevc_fei/include/sample_hevc_fei_defs.h
@@ -55,6 +55,16 @@ struct SourceFrameInfo
     }
 };
 
+struct PerFrameTypeCtrl
+{
+    mfxExtFeiHevcEncFrameCtrl   CtrlI;
+    mfxExtFeiHevcEncFrameCtrl   CtrlP; // also applicable for GPB frames
+    mfxExtFeiHevcEncFrameCtrl   CtrlB;
+
+    PerFrameTypeCtrl() : CtrlI(), CtrlP(), CtrlB()
+    {}
+};
+
 struct sInputParams
 {
     SourceFrameInfo input;
@@ -101,6 +111,7 @@ struct sInputParams
 
     mfxExtFeiPreEncCtrl         preencCtrl;
     mfxExtFeiHevcEncFrameCtrl   encodeCtrl;
+    PerFrameTypeCtrl            frameCtrl;
 
     sInputParams()
         : bENCODE(false)
@@ -172,6 +183,9 @@ struct sInputParams
         //set default values for NumMvPredictors
         encodeCtrl.NumMvPredictors[0] = 3;
         encodeCtrl.NumMvPredictors[1] = 1;
+
+        frameCtrl.CtrlI = frameCtrl.CtrlP = frameCtrl.CtrlB = encodeCtrl;
+        frameCtrl.CtrlI.MVPredictor = 0;
     }
 
 };

samples/sample_hevc_fei/src/hevc_fei_encode.cpp
@@ -21,7 +21,8 @@ or https://software.intel.com/en-us/media-client-solutions-support.
 #include "hevc_fei_encode.h"
 
 FEI_Encode::FEI_Encode(MFXVideoSession* session, mfxHDL hdl,
-        MfxVideoParamsWrapper& encode_pars, const mfxExtFeiHevcEncFrameCtrl& encodeCtrl,
+        MfxVideoParamsWrapper& encode_pars,
+        const mfxExtFeiHevcEncFrameCtrl& frameCtrl, const PerFrameTypeCtrl& frametypeCtrl,
         const msdk_char* strDstFile, const msdk_char* mvpInFile,
         const msdk_char* repackctrlFile, const msdk_char* repackstatFile,
         PredictorsRepaking* repacker)
@@ -31,7 +32,8 @@ FEI_Encode::FEI_Encode(MFXVideoSession* session, mfxHDL hdl,
     , m_videoParams(encode_pars)
     , m_syncPoint(0)
     , m_dstFileName(strDstFile)
-    , m_defFrameCtrl(encodeCtrl)
+    , m_defFrameCtrl(frameCtrl)
+    , m_ctrlPerFrameType(frametypeCtrl)
     , m_processedFrames(0)
     , m_repackCtrlFileName(repackctrlFile)
     , m_repackStatFileName(repackstatFile)
@@ -431,6 +433,28 @@ mfxStatus FEI_Encode::SetCtrlParams(const HevcTask& task)
                 ctrl->NumMvPredictors[1] = m_defFrameCtrl.NumMvPredictors[1];
             }
         }
+    }
+
+    switch (m_encodeCtrl.FrameType  & (MFX_FRAMETYPE_I | MFX_FRAMETYPE_P | MFX_FRAMETYPE_B))
+    {
+        case MFX_FRAMETYPE_I:
+            ctrl->FastIntraMode      = m_ctrlPerFrameType.CtrlI.FastIntraMode;
+            ctrl->ForceCtuSplit      = m_ctrlPerFrameType.CtrlI.ForceCtuSplit;
+            ctrl->NumFramePartitions = m_ctrlPerFrameType.CtrlI.NumFramePartitions;
+            break;
+        case MFX_FRAMETYPE_P:
+            ctrl->FastIntraMode      = m_ctrlPerFrameType.CtrlP.FastIntraMode;
+            ctrl->ForceCtuSplit      = m_ctrlPerFrameType.CtrlP.ForceCtuSplit;
+            ctrl->NumFramePartitions = m_ctrlPerFrameType.CtrlP.NumFramePartitions;
+            break;
+        case MFX_FRAMETYPE_B:
+            ctrl->FastIntraMode      = m_ctrlPerFrameType.CtrlB.FastIntraMode;
+            ctrl->ForceCtuSplit      = m_ctrlPerFrameType.CtrlB.ForceCtuSplit;
+            ctrl->NumFramePartitions = m_ctrlPerFrameType.CtrlB.NumFramePartitions;
+            break;
+        default:
+            throw mfxError(MFX_ERR_UNDEFINED_BEHAVIOR, "Invalid m_encodeCtrl.FrameType");
+            break;
     }
 
     return MFX_ERR_NONE;

samples/sample_hevc_fei/src/pipeline_hevc_fei.cpp
@@ -627,7 +627,7 @@ FEI_Encode* CEncodingPipeline::CreateEncode(mfxFrameInfo& in_fi)
     CHECK_STS_AND_RETURN(sts, "CreateEncode::m_pHWdev->GetHandle failed", NULL);
 
     FEI_Encode* pEncode = new FEI_Encode(&m_mfxSession, hdl,
-                                         pars, m_inParams.encodeCtrl,
+                                         pars, m_inParams.encodeCtrl, m_inParams.frameCtrl,
                                          m_inParams.strDstFile, m_inParams.mvpInFile,
                                          m_inParams.repackctrlFile, m_inParams.repackstatFile,
                                          pRepacker.get());

commit 554d0a538b9ea6017c5a37a1bccf3d2d8fab4923

[sample_hevc_fei] Fix regression caused by 29d17bd
For display order (e.g. pipeline w/o preenc) reordering is
performed in library and SetCtrlParams is not called, so
there is no way to use per type controls for setting
ForceCtuSplit and FastIntraMode. Change restores setting them
as before commit 29d17bd.

samples/sample_hevc_fei/src/sample_hevc_fei.cpp
@@ -424,7 +424,8 @@ mfxStatus ParseInputString(msdk_char* strInput[], mfxU32 nArgNum, sInputParams& 
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit")))
         {
-            params.frameCtrl.CtrlI.ForceCtuSplit = params.frameCtrl.CtrlP.ForceCtuSplit = params.frameCtrl.CtrlB.ForceCtuSplit = 1;
+            params.encodeCtrl.ForceCtuSplit = params.frameCtrl.CtrlI.ForceCtuSplit =
+                params.frameCtrl.CtrlP.ForceCtuSplit = params.frameCtrl.CtrlB.ForceCtuSplit = 1;
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-ForceCtuSplit:I")))
         {
@@ -464,9 +465,8 @@ mfxStatus ParseInputString(msdk_char* strInput[], mfxU32 nArgNum, sInputParams& 
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntraMode")))
         {
-            params.frameCtrl.CtrlI.FastIntraMode =
-                params.frameCtrl.CtrlP.FastIntraMode =
-                params.frameCtrl.CtrlB.FastIntraMode = 1;
+            params.encodeCtrl.FastIntraMode = params.frameCtrl.CtrlI.FastIntraMode =
+                params.frameCtrl.CtrlP.FastIntraMode = params.frameCtrl.CtrlB.FastIntraMode = 1;
         }
         else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-FastIntra:I")))
         {
@@ -824,14 +824,37 @@ void AdjustOptions(sInputParams& params)
         params.bEncodedOrder = true;
     }
 
-    if (!params.bExtBRC && params.TargetKbps) {
+    if (!params.bExtBRC && params.TargetKbps)
+    {
         msdk_printf(MSDK_STRING("WARNING: Target bitrate is ignored as external BRC is disabled\n"));
         params.TargetKbps = 0;
     }
 
-    if (!params.bExtBRC && !params.QP) {
+    if (!params.bExtBRC && !params.QP)
+    {
         msdk_printf(MSDK_STRING("WARNING: QP is not specified. Adjust to 26 (default)\n"));
         params.QP = 26;
+    }
+
+    if (!params.bEncodedOrder && !params.encodeCtrl.FastIntraMode &&
+            (params.frameCtrl.CtrlI.FastIntraMode || params.frameCtrl.CtrlP.FastIntraMode || params.frameCtrl.CtrlB.FastIntraMode))
+    {
+        msdk_printf(MSDK_STRING("WARNING: -FastIntra:I, -FastIntra:P and -FastIntra:B are not supported in display order\n"));
+        params.frameCtrl.CtrlI.FastIntraMode = params.frameCtrl.CtrlP.FastIntraMode = params.frameCtrl.CtrlB.FastIntraMode = 0;
+    }
+    if (!params.bEncodedOrder && !params.encodeCtrl.ForceCtuSplit &&
+            (params.frameCtrl.CtrlI.ForceCtuSplit || params.frameCtrl.CtrlP.ForceCtuSplit || params.frameCtrl.CtrlB.ForceCtuSplit))
+    {
+        msdk_printf(MSDK_STRING("WARNING: -ForceCtuSplit:I, -ForceCtuSplit:P and -ForceCtuSplit:B are not supported in display order\n"));
+        params.frameCtrl.CtrlI.ForceCtuSplit = params.frameCtrl.CtrlP.ForceCtuSplit = params.frameCtrl.CtrlB.ForceCtuSplit = 0;
+    }
+    if (!params.bEncodedOrder && (params.encodeCtrl.NumFramePartitions != params.frameCtrl.CtrlI.NumFramePartitions ||
+            params.encodeCtrl.NumFramePartitions != params.frameCtrl.CtrlP.NumFramePartitions ||
+            params.encodeCtrl.NumFramePartitions != params.frameCtrl.CtrlB.NumFramePartitions))
+    {
+        msdk_printf(MSDK_STRING("WARNING: -NFP:I, -NFP:P and -NFP:B are not supported in display order\n"));
+        params.frameCtrl.CtrlI.NumFramePartitions = params.frameCtrl.CtrlP.NumFramePartitions =
+            params.frameCtrl.CtrlB.NumFramePartitions = params.encodeCtrl.NumFramePartitions;
     }
 }
 