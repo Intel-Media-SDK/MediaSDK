e2bfd004ce6bc3577537bce6002f3e52445a2e1a
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2018-11-07 17:56:39 +0300

    AVCd: remove MFX_MIN/MAX macro
    Replaced with std::min/max

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_dec_bitstream_headers.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_dec_bitstream_headers.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -1292,7 +1292,7 @@ Status H264HeadersBitstream::GetPictureParamSetPart2(H264PicParamSet  *pps, H264
                 if (pps->SliceGroupInfo.t3.pic_size_in_map_units != PicSizeInMapUnits)
                     return UMC_ERR_INVALID_STREAM;
 
-                uint32_t const len = MFX_MAX(1, pps->SliceGroupInfo.t3.pic_size_in_map_units);
+                uint32_t const len = std::max(1u, pps->SliceGroupInfo.t3.pic_size_in_map_units);
 
                 pps->SliceGroupInfo.pSliceGroupIDMap.resize(len);
 
