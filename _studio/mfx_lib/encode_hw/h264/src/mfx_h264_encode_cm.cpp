e7a9a111a0988819f6e9ec2a4a07f88861f5a9ad
Artem Shaporenko <artem.shaporenko@intel.com>
2018-11-08 19:25:49 +0300

    add lookahead execution with SSEU on ICL
    Signed-off-by: Artem Shaporenko <artem.shaporenko@intel.com>

--- _studio/mfx_lib/encode_hw/h264/src/mfx_h264_encode_cm.cpp
+++ _studio/mfx_lib/encode_hw/h264/src/mfx_h264_encode_cm.cpp
@@ -840,9 +840,13 @@ void CmContext::Setup(
 {
     m_video  = video;
     m_device = cmDevice;
-
-    if (m_device->CreateQueue(m_queue) != CM_SUCCESS)
-        throw CmRuntimeError();
+    if (core->GetHWType() >= MFX_HW_ICL)
+    {
+        if (m_device->CreateQueueEx(m_queue, CM_VME_QUEUE_CREATE_OPTION) != CM_SUCCESS)
+            throw CmRuntimeError();
+    }
+    else if (m_device->CreateQueue(m_queue) != CM_SUCCESS)
+            throw CmRuntimeError();
 
     mfxExtCodingOption2 const * extOpt2 = GetExtBuffer(m_video);
     if (!extOpt2)
