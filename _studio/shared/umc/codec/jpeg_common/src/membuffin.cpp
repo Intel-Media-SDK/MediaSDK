fc9364b6af21585576eff340a935c371a778f50e
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2018-11-12 17:53:27 +0300

    jpeg: remove MFX_MIN/MAX macro
    Replaced with std::min/max

--- _studio/shared/umc/codec/jpeg_common/src/membuffin.cpp
+++ _studio/shared/umc/codec/jpeg_common/src/membuffin.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -94,9 +94,7 @@ JERRCODE CMemBuffInput::Seek(long offset, int origin)
 
 JERRCODE CMemBuffInput::Read(void* buf,uic_size_t len,uic_size_t* cnt)
 {
-  uic_size_t rb;
-
-  rb = (uic_size_t)MFX_MIN((int)len,static_cast<int>(m_buflen - m_currpos));
+  uic_size_t rb = std::min<uic_size_t>(len, m_buflen - m_currpos);
 
   MFX_INTERNAL_CPY((uint8_t*)buf, m_buf + m_currpos,rb);
 
