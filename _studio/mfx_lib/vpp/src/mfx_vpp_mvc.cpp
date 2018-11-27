ce7a7709932b7e88390b1ad5dc9da8b950f88535
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2018-11-27 19:23:34 +0300

    vpp mvc: removed using namespace from header

--- _studio/mfx_lib/vpp/src/mfx_vpp_mvc.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_mvc.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Intel Corporation
+// Copyright (c) 2017-2018 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -127,7 +127,7 @@ mfxStatus ImplementationMvc::Init(mfxVideoParam *par)
             mfxSts = MFX_ERR_NONE;
         }
 
-        pair<mfxMultiViewVPP_Iterator, bool> insertSts = m_VPP.insert( make_pair(viewId, pNewVPP) );
+        std::pair<mfxMultiViewVPP_Iterator, bool> insertSts = m_VPP.insert( std::make_pair(viewId, pNewVPP) );
         if( !insertSts.second )
         {
             return MFX_ERR_UNDEFINED_BEHAVIOR;
