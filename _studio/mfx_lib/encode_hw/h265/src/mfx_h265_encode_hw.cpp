e6a8257e9f2fb768cbfb5a748ce004193c9b6256
root <root@nnt-msdk-icl-005.inn.intel.com>
2018-12-11 14:41:51 +0000

    hevce: delete pitch limitation
    The check is no more relevant

--- _studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw.cpp
+++ _studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw.cpp
@@ -854,7 +854,6 @@ mfxStatus  MFXVideoENCODEH265_HW::EncodeFrameSubmit(mfxEncodeCtrl *ctrl, mfxFram
     if (surface)
     {
         MFX_CHECK(LumaIsNull(surface) == (surface->Data.UV == 0), MFX_ERR_UNDEFINED_BEHAVIOR);
-        MFX_CHECK(surface->Data.Pitch < 0x8000, MFX_ERR_UNDEFINED_BEHAVIOR);
         MFX_CHECK(surface->Info.Width  >=  m_vpar.mfx.FrameInfo.Width, MFX_ERR_INVALID_VIDEO_PARAM);
         MFX_CHECK(surface->Info.Height >=  m_vpar.mfx.FrameInfo.Height, MFX_ERR_INVALID_VIDEO_PARAM);
     }
