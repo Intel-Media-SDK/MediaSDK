bfd932f84245b0c30c32caf97676667e24c38432
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-12-20 23:06:06 +0300

    Fix build for API 1.26
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- _studio/mfx_lib/vpp/src/mfx_vpp_utils.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_utils.cpp
@@ -1311,6 +1311,7 @@ mfxU32 GetFilterIndex( mfxU32* pList, mfxU32 len, mfxU32 filterName )
 /* check each field of FrameInfo excluding PicStruct */
 mfxStatus CheckFrameInfo(mfxFrameInfo* info, mfxU32 request, eMFXHWType platform)
 {
+    (void)platform;
     mfxStatus mfxSts = MFX_ERR_NONE;
 
     /* FourCC */
