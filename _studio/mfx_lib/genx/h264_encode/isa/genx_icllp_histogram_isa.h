0591c54751480a2282852f00c6f2fa9423687cb3
Artem Shaporenko <artem.shaporenko@intel.com>
2018-12-20 15:38:59 +0300

    rebuild kernels with fresh compiler fix ICL kernels

--- _studio/mfx_lib/genx/h264_encode/isa/genx_icllp_histogram_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_icllp_histogram_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2012-2018 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,5 +20,5 @@
 
 #ifndef __genx_icllp_histogram__
 #define __genx_icllp_histogram__
-extern const unsigned char genx_icllp_histogram[76146];
+extern const unsigned char genx_icllp_histogram[63286];
 #endif
