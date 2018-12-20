49e9f58da96da4a756d6c47980385cf13be87037
Artem Shaporenko <artem.shaporenko@intel.com>
2018-12-20 16:14:02 +0300

    Rebuild kernels with new compiler Fix ICL kernels, improve previous platforms
    Signed-off-by: Artem Shaporenko <artem.shaporenko@intel.com>

--- _studio/mfx_lib/genx/h264_encode/isa/genx_cnl_histogram_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_cnl_histogram_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2012-2018 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,5 +20,5 @@
 
 #ifndef __genx_cnl_histogram__
 #define __genx_cnl_histogram__
-extern const unsigned char genx_cnl_histogram[76034];
+extern const unsigned char genx_cnl_histogram[62934];
 #endif
