22c5f65acab544e52d5785d3ee304f9e433da5dd
Dmitry Sorokin <dmitry.sorokin@intel.com>
2019-01-14 19:58:06 +0300

    genx/la: add kernels build support

--- _studio/mfx_lib/genx/h264_encode/isa/genx_simple_me_gen9_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_simple_me_gen9_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2012-2018 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,7 +17,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-#ifndef __genx_bdw_simple_me__
-#define __genx_bdw_simple_me__
-extern const unsigned char genx_bdw_simple_me[259932];
+
+#ifndef __genx_simple_me_gen9__
+#define __genx_simple_me_gen9__
+extern const unsigned char genx_simple_me_gen9[259428];
 #endif
