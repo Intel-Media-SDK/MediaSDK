7ed41b15f7c96317fa080d1c46063edb5596b2f0
Artem Shaporenko <artem.shaporenko@intel.com>
2019-01-14 15:56:52 +0300

    Fix BDW Lookahead kernel
    Signed-off-by: Artem Shaporenko <artem.shaporenko@intel.com>

--- _studio/mfx_lib/genx/h264_encode/isa/genx_bdw_simple_me_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_bdw_simple_me_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +19,5 @@
 // SOFTWARE.
 #ifndef __genx_bdw_simple_me__
 #define __genx_bdw_simple_me__
-extern const unsigned char genx_bdw_simple_me[252603];
+extern const unsigned char genx_bdw_simple_me[259932];
 #endif
