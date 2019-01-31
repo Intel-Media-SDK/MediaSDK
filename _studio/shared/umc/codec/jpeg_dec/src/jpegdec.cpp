a55c2582f74fb7454a4573897bb3585466ccf5f1
Andrey Orlov <andrey.orlov@intel.com>
2019-01-31 17:06:48 +0300

    Valgrind issue fix: Mismatched free() / delete / delete [] in jpeg decode
    Issue: MDP-53172

--- _studio/shared/umc/codec/jpeg_dec/src/jpegdec.cpp
+++ _studio/shared/umc/codec/jpeg_dec/src/jpegdec.cpp
@@ -1926,7 +1926,7 @@ JERRCODE CJPEGDecoder::UpSampling(uint32_t rowMCU, uint32_t colMCU, uint32_t max
           // set the pointer to source buffer
           pSrc = curr_comp->GetSSBufferPtr<uint8_t> (0) + 8 * colMCU * curr_comp->m_scan_hsampling;
           // set the pointer to temporary buffer
-          std::unique_ptr<uint8_t> pTmp( new uint8_t[2 * srcWidth / m_dd_factor] );
+          std::unique_ptr<uint8_t[]> pTmp( new uint8_t[2 * srcWidth / m_dd_factor] );
           // set the pointer to destination buffer
           pDst = curr_comp->GetCCBufferPtr<uint8_t> (0) + 8 * colMCU * curr_comp->m_h_factor;
          
@@ -2053,7 +2053,7 @@ JERRCODE CJPEGDecoder::UpSampling(uint32_t rowMCU, uint32_t colMCU, uint32_t max
             // set the pointer to source buffer
             pSrc = curr_comp->GetCCBufferPtr<uint8_t> (0) + 8 * colMCU;
             // set the pointer to temporary buffer
-            std::unique_ptr<uint8_t> pTmp( new uint8_t[tmpStep * m_curr_scan->mcuHeight / 2] );
+            std::unique_ptr<uint8_t[]> pTmp( new uint8_t[tmpStep * m_curr_scan->mcuHeight / 2] );
             // set the pointer to destination buffer
             pDst = curr_comp->GetCCBufferPtr<uint8_t> (0) + 8 * colMCU / 2;  
 
