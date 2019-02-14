Intel-Media-SDK/MediaSDK

commit be48bc3524e2064353582023ce9fac471a9f9624

[VP9e] Added dynamic scaling flag
For now dynamic scaling is always ON so it leads
to additional allocation on driver side to support
such scenario. Was added key app should set
if dynamic scaling feature is needed.

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw.cpp
@@ -471,6 +471,14 @@ mfxStatus MFXVideoENCODEVP9_HW::Reset(mfxVideoParam *par)
         {
             return MFX_ERR_INVALID_VIDEO_PARAM;
         }
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        if ((extParAfter.FrameWidth != extParBefore.FrameWidth ||
+            extParAfter.FrameHeight != extParBefore.FrameHeight) &&
+            extParAfter.DynamicScaling != MFX_CODINGOPTION_ON)
+        {
+            return MFX_ERR_INVALID_VIDEO_PARAM;
+        }
+#endif
     }
 
     sts = m_ddi->Reset(m_video);

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw_par.cpp
@@ -190,6 +190,7 @@ inline void SetOrCopy(mfxExtVP9Param *pDst, mfxExtVP9Param const *pSrc = 0, bool
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     SET_OR_COPY_PAR(NumTileRows);
     SET_OR_COPY_PAR(NumTileColumns);
+    SET_OR_COPY_PAR(DynamicScaling);
 #endif
 }
 
@@ -1530,6 +1531,22 @@ mfxStatus CheckParameters(VP9MfxVideoParam &par, ENCODE_CAPS_VP9 const &caps)
         rows = 0;
         unsupported = true;
     }
+
+    if (false == CheckTriStateOption(extPar.DynamicScaling))
+    {
+        changed = true;
+    }
+    if (!caps.DynamicScaling && extPar.DynamicScaling == MFX_CODINGOPTION_ON)
+    {
+        extPar.DynamicScaling = MFX_CODINGOPTION_OFF;
+        unsupported = true;
+    }
+    //known limitation: these 2 features don't work together
+    if (extPar.DynamicScaling && par.m_numLayers > 0)
+    {
+        extPar.DynamicScaling = MFX_CODINGOPTION_OFF;
+        unsupported = true;
+    }
 #endif // MFX_VERSION >= MFX_VERSION_NEXT
 
     return GetCheckStatus(changed, unsupported);
@@ -1726,6 +1743,7 @@ mfxStatus SetDefaults(
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     SetDefault(extPar.NumTileColumns, (extPar.FrameWidth + MAX_TILE_WIDTH - 1) / MAX_TILE_WIDTH);
     SetDefault(extPar.NumTileRows, 1);
+    SetDefault(extPar.DynamicScaling, MFX_CODINGOPTION_OFF);
 #endif // (MFX_VERSION >= MFX_VERSION_NEXT)
 
     // ext buffers

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw_vaapi.cpp
@@ -413,6 +413,13 @@ mfxStatus SetRateControl(
             rate_param->rc_flags.bits.temporal_id = tl;
         }
 
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        mfxExtVP9Param vp9param = GetExtBufferRef(par);
+        rate_param->rc_flags.bits.enable_dynamic_scaling = vp9param.DynamicScaling == MFX_CODINGOPTION_ON ? 1 : 0;
+#else
+        rate_param->rc_flags.bits.enable_dynamic_scaling = 1;
+#endif
+
         vaSts = vaUnmapBuffer(m_vaDisplay, rateParamBuf_ids[tl]);
         MFX_CHECK_WITH_ASSERT(VA_STATUS_SUCCESS == vaSts, MFX_ERR_DEVICE_FAILED);
     }

api/include/mfxstructures.h
@@ -2034,7 +2034,8 @@ typedef struct {
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     mfxU16  NumTileRows;
     mfxU16  NumTileColumns;
-    mfxU16  reserved[110];
+    mfxU16  DynamicScaling;
+    mfxU16  reserved[109];
 #else // API 1.26
     mfxU16  reserved[112];
 #endif

api/mediasdk_structures/ts_struct_decl.h
@@ -1266,6 +1266,7 @@ STRUCT(mfxExtVP9Param,
     FIELD_T(mfxI16      , QIndexDeltaChromaDC)
     FIELD_T(mfxU16      , NumTileRows)
     FIELD_T(mfxU16      , NumTileColumns)
+    FIELD_T(mfxU16      , DynamicScaling)
 )
 #elif (MFX_VERSION >= 1026)
 STRUCT(mfxExtVP9Param,

commit 77c777593b3051761c181f6e08d40bbe71e314e2

[VP9e] Reverted changes made to DynamicScaling
Fixes: be48bc3 ("[VP9e] Added dynamic scaling flag")

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -468,14 +468,25 @@ mfxStatus MFXVideoENCODEVP9_HW::Reset(mfxVideoParam *par)
         {
             return MFX_ERR_INVALID_VIDEO_PARAM;
         }
+
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
+        // dynamic scaling and tiles don't work together
         if ((extParAfter.FrameWidth != extParBefore.FrameWidth ||
             extParAfter.FrameHeight != extParBefore.FrameHeight) &&
-            extParAfter.DynamicScaling != MFX_CODINGOPTION_ON)
+            (extParAfter.NumTileRows > 1 || extParAfter.NumTileColumns > 1))
         {
             return MFX_ERR_INVALID_VIDEO_PARAM;
         }
 #endif
+
+        // dynamic scaling and segmentation don't work together
+        const mfxExtVP9Segmentation& seg = GetExtBufferRef(parAfterReset);
+        if ((extParAfter.FrameWidth != extParBefore.FrameWidth ||
+            extParAfter.FrameHeight != extParBefore.FrameHeight) &&
+            seg.NumSegments > 1)
+        {
+            return MFX_ERR_INVALID_VIDEO_PARAM;
+        }
     }
 
     sts = m_ddi->Reset(m_video);

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw_par.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -189,7 +189,6 @@ inline void SetOrCopy(mfxExtVP9Param *pDst, mfxExtVP9Param const *pSrc = 0, bool
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     SET_OR_COPY_PAR(NumTileRows);
     SET_OR_COPY_PAR(NumTileColumns);
-    SET_OR_COPY_PAR(DynamicScaling);
 #endif
 }
 
@@ -1537,19 +1536,10 @@ mfxStatus CheckParameters(VP9MfxVideoParam &par, ENCODE_CAPS_VP9 const &caps)
         unsupported = true;
     }
 
-    if (false == CheckTriStateOption(extPar.DynamicScaling))
+    // known limitation: temporal scalability and tiles don't work together
+    if (par.m_numLayers > 1 && (rows > 1 || cols > 1))
     {
-        changed = true;
-    }
-    if (!caps.DynamicScaling && extPar.DynamicScaling == MFX_CODINGOPTION_ON)
-    {
-        extPar.DynamicScaling = MFX_CODINGOPTION_OFF;
-        unsupported = true;
-    }
-    //known limitation: these 2 features don't work together
-    if (extPar.DynamicScaling == MFX_CODINGOPTION_ON && par.m_numLayers > 0)
-    {
-        extPar.DynamicScaling = MFX_CODINGOPTION_OFF;
+        rows = cols = 1;
         unsupported = true;
     }
 #endif // MFX_VERSION >= MFX_VERSION_NEXT
@@ -1745,7 +1735,6 @@ mfxStatus SetDefaults(
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     SetDefault(extPar.NumTileColumns, (extPar.FrameWidth + MAX_TILE_WIDTH - 1) / MAX_TILE_WIDTH);
     SetDefault(extPar.NumTileRows, 1);
-    SetDefault(extPar.DynamicScaling, MFX_CODINGOPTION_OFF);
 #endif // (MFX_VERSION >= MFX_VERSION_NEXT)
 
     // ext buffers

_studio/mfx_lib/encode_hw/vp9/src/mfx_vp9_encode_hw_vaapi.cpp
@@ -415,12 +415,7 @@ mfxStatus SetRateControl(
             rate_param->rc_flags.bits.temporal_id = tl;
         }
 
-#if (MFX_VERSION >= MFX_VERSION_NEXT)
-        mfxExtVP9Param vp9param = GetExtBufferRef(par);
-        rate_param->rc_flags.bits.enable_dynamic_scaling = vp9param.DynamicScaling == MFX_CODINGOPTION_ON ? 1 : 0;
-#else
         rate_param->rc_flags.bits.enable_dynamic_scaling = 1;
-#endif
 
         vaSts = vaUnmapBuffer(m_vaDisplay, rateParamBuf_ids[tl]);
         MFX_CHECK_WITH_ASSERT(VA_STATUS_SUCCESS == vaSts, MFX_ERR_DEVICE_FAILED);

api/include/mfxstructures.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -2019,8 +2019,7 @@ typedef struct {
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     mfxU16  NumTileRows;
     mfxU16  NumTileColumns;
-    mfxU16  DynamicScaling;
-    mfxU16  reserved[109];
+    mfxU16  reserved[110];
 #else // API 1.26
     mfxU16  reserved[112];
 #endif

api/mediasdk_structures/ts_struct_decl.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -1254,7 +1254,6 @@ STRUCT(mfxExtVP9Param,
     FIELD_T(mfxI16      , QIndexDeltaChromaDC)
     FIELD_T(mfxU16      , NumTileRows)
     FIELD_T(mfxU16      , NumTileColumns)
-    FIELD_T(mfxU16      , DynamicScaling)
 )
 #elif (MFX_VERSION >= 1026)
 STRUCT(mfxExtVP9Param,