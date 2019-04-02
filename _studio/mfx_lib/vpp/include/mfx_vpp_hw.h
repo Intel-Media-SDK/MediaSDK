900bb502bae7c5d9a95ce9ab7be8db0dfd0745c0
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2019-04-02 12:06:51 +0300

    vpp: remove auto_ptr traces from comments

--- _studio/mfx_lib/vpp/include/mfx_vpp_hw.h
+++ _studio/mfx_lib/vpp/include/mfx_vpp_hw.h
@@ -963,7 +963,7 @@ namespace MfxHwVideoProcessing
         mfxU32        m_frame_num;
         mfxStatus     m_critical_error;
 
-        // Not an auto_ptr anymore since core owns create/delete semantic now.
+        // Not a smart pointer anymore since core owns create/delete semantic now.
         VPPHWResMng * m_ddi;
         bool          m_bMultiView;
 
