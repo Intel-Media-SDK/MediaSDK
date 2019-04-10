0c6c9eb30715f104d26ddf3bdd31271fa1e8a60b
fzhar <Fedor.Zharinov@intel.com>
2019-04-10 15:49:50 +0300

    std::max issue is fixed in samples

--- samples/sample_common/include/brc_routines.h
+++ samples/sample_common/include/brc_routines.h
@@ -17,6 +17,7 @@ The original version of this sample may be obtained from https://software.intel.
 or https://software.intel.com/en-us/media-client-solutions-support.
 \**********************************************************************************/
 #include "sample_defs.h"
+#include "algorithm"
 
 #ifndef MFX_VERSION
 #error MFX_VERSION not defined
