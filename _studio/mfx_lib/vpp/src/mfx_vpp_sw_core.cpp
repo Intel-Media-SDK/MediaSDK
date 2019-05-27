455764a9ac7a08010be5c35dfaa5f64be805c62b
Emelianova Svetlana <svetlana.emelianova@intel.com>
2019-05-27 13:01:01 +0300

    [VPP] Fix to check status

--- _studio/mfx_lib/vpp/src/mfx_vpp_sw_core.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_sw_core.cpp
@@ -57,7 +57,10 @@ VideoVPPBase* CreateAndInitVPPImpl(mfxVideoParam *par, VideoCORE *core, mfxStatu
             return 0;
         }
 
-        if(MFX_WRN_INCOMPATIBLE_VIDEO_PARAM == *mfxSts || MFX_WRN_FILTER_SKIPPED == *mfxSts || MFX_ERR_NONE == *mfxSts)
+        if(MFX_WRN_INCOMPATIBLE_VIDEO_PARAM == *mfxSts ||
+            MFX_WRN_FILTER_SKIPPED == *mfxSts ||
+            MFX_WRN_PARTIAL_ACCELERATION == *mfxSts ||
+            MFX_ERR_NONE == *mfxSts)
         {
             return vpp;
         }
@@ -1082,8 +1085,7 @@ mfxStatus VideoVPPBase::Query(VideoCORE * core, mfxVideoParam *in, mfxVideoParam
         mfxSts = CheckPlatformLimitations(core, *out, bCorrectionEnable);
         //-------------------------------------------------
 
-        mfxStatus   hwQuerySts = MFX_ERR_NONE,
-                    swQuerySts = MFX_ERR_NONE;
+        mfxStatus   hwQuerySts = MFX_ERR_NONE;
 
         if(MFX_PLATFORM_HARDWARE == core->GetPlatformType())
         {
@@ -1110,13 +1112,12 @@ mfxStatus VideoVPPBase::Query(VideoCORE * core, mfxVideoParam *in, mfxVideoParam
                 hwQuerySts = MFX_WRN_PARTIAL_ACCELERATION;
             }
         }
+        else
+        {
+            MFX_RETURN(MFX_ERR_UNSUPPORTED);
+        }
 
-        MFX_CHECK_STS(MFX_ERR_UNSUPPORTED);
-
-        if (hwQuerySts != MFX_ERR_NONE)
-            return hwQuerySts;
-        if (swQuerySts != MFX_ERR_NONE)
-            return swQuerySts;
+        MFX_CHECK_STS(hwQuerySts);
 
         return mfxSts;
     }//else
