19ffae22e154a6e4108e69a2420572904001d7b0
Haihao Xiang <haihao.xiang@intel.com>
2019-08-02 10:01:45 +0800

    Allow a larger surface for mpeg2/jpeg encoding
    Like other CODECs, we may allow a larger surface for mpeg2/jpeg
    encoding. If do so, we may use the same way to allocate surfaces for all
    CODECs in user application / library.

--- _studio/mfx_lib/shared/include/mfx_enc_common.h
+++ _studio/mfx_lib/shared/include/mfx_enc_common.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -62,10 +62,6 @@ public:
 
      inline bool isOpaq() {return  m_bOpaq;}
      inline bool isSysMemFrames () {return m_bSysMemFrames;}
-     inline bool CheckInputFrame(mfxFrameInfo* pFrameInfo)
-     {
-         return (m_Info.Width == pFrameInfo->Width && m_Info.Height == pFrameInfo->Height);     
-     }
 
      inline mfxFrameSurface1 *GetOriginalSurface(mfxFrameSurface1 *surface)
      {
