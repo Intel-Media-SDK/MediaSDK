e498906f5a0426b12b30bf2754943aae9e987114
egorovdanil <danil.egorov@intel.com>
2019-09-12 15:14:26 +0300

    relocation of static functions to source files

--- api/mfx_dispatch/linux/mfxparser.cpp
+++ api/mfx_dispatch/linux/mfxparser.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -45,6 +45,22 @@ static bool parseGUID(const char* src, mfxPluginUID* uid)
 
     *uid = plugin_uid;
     return true;
+}
+
+static std::string printUID(const mfxPluginUID& uid)
+{
+  std::stringstream ss;
+  ss << std::hex;
+  for (auto c: uid.Data) ss << static_cast<unsigned>(c);
+  return ss.str();
+}
+
+static std::string printCodecId(mfxU32 id)
+{
+  uint8_t* data = reinterpret_cast<uint8_t*>(&id);
+  std::stringstream ss;
+  for (size_t i=0; i < sizeof(id); ++i) ss << data[i];
+  return ss.str();
 }
 
 void PluginInfo::Load(const char* name, const char* value)
