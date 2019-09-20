Intel-Media-SDK/MediaSDK

commit 4f328187b7cfd88849dad6a1bb905cbd01ec9792

[mpeg2d] Invalid slices handling
1) Handling case when vertical_position of slice is out of range
2) Recalculating numberMBsInSlice of prev slice if two
slices are located at the same row
3) Do not call Submit() if sliceCount = 0

_studio/shared/umc/codec/mpeg2_dec/src/umc_mpeg2_bitstream.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,7 @@
 // SOFTWARE.
 
 #include "umc_defs.h"
+#include "mfx_utils.h"
 #if defined (MFX_ENABLE_MPEG2_VIDEO_DECODE)
 
 #include "umc_mpeg2_defs.h"
@@ -294,6 +295,15 @@ namespace UMC_MPEG2_DECODER
 
         if (CheckBSLeft())
             throw mpeg2_exception(UMC::UMC_ERR_INVALID_STREAM);
+
+        uint32_t width_in_MBs  = mfx::align2_value<uint32_t>(slice.GetSeqHeader().horizontal_size_value, 16) / 16u;
+        uint32_t height_in_MBs = mfx::align2_value<uint32_t>(slice.GetSeqHeader().vertical_size_value, 16) / 16u;
+
+        // invalid slice - skipping it
+        if (slice.sliceHeader.slice_vertical_position >= height_in_MBs)
+            throw mpeg2_exception(UMC::UMC_ERR_INVALID_PARAMS);
+
+        slice.sliceHeader.numberMBsInSlice = width_in_MBs - slice.sliceHeader.macroblockAddressIncrement;
 
         return UMC::UMC_OK;
     }

_studio/shared/umc/codec/mpeg2_dec/include/umc_mpeg2_decoder.h
@@ -187,6 +187,7 @@ namespace UMC_MPEG2_DECODER
 
         // Action on new picture
         virtual bool OnNewPicture(); // returns true on full frame
+        virtual void EliminateSliceErrors(MPEG2DecoderFrame& frame, uint8_t fieldIndex);
         virtual UMC::Status CompletePicture(MPEG2DecoderFrame& frame, uint8_t fieldIndex);
         bool IsFieldOfCurrentFrame() const;
 

_studio/shared/umc/codec/mpeg2_dec/include/umc_mpeg2_defs.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -248,6 +248,7 @@ namespace UMC_MPEG2_DECODER
         // calculated params
         uint32_t mbOffset                         = 0;
         uint32_t macroblockAddressIncrement       = 0;
+        uint32_t numberMBsInSlice                 = 0; // only for DXVA
     };
 
     enum

_studio/shared/umc/codec/mpeg2_dec/src/umc_mpeg2_decoder.cpp
@@ -903,6 +903,32 @@ namespace UMC_MPEG2_DECODER
         return false;
     }
 
+    void MPEG2Decoder::EliminateSliceErrors(MPEG2DecoderFrame& frame, uint8_t fieldIndex)
+    {
+        MPEG2DecoderFrameInfo & frameInfo = *frame.GetAU(fieldIndex);
+        size_t sliceCount = frameInfo.GetSliceCount();
+
+        for (size_t sliceNum = 0; sliceNum < sliceCount; sliceNum++)
+        {
+            auto slice = frameInfo.GetSlice(sliceNum);
+            auto sliceHeader = slice->GetSliceHeader();
+
+            // Slice may cover just a part of a row (not full row), this is indicated by macroblockAddressIncrement
+            // Looping over slices, we heed to check if next slice has macroblockAddressIncrement > 0,
+            // this means that we need to recalculate numberMBsInSlice for previous slice
+            if (sliceNum > 0 && sliceHeader.macroblockAddressIncrement > 0)
+            {
+                auto prevSlice = frameInfo.GetSlice(sliceNum - 1); //take previous
+                auto& prevSliceHeader = prevSlice->GetSliceHeader();
+
+                // Check if this parts are located at the same row
+                if (sliceHeader.slice_vertical_position == prevSliceHeader.slice_vertical_position)
+                    prevSliceHeader.numberMBsInSlice = sliceHeader.macroblockAddressIncrement - prevSliceHeader.macroblockAddressIncrement;
+            }
+        }
+        return;
+    }
+
     UMC::Status MPEG2Decoder::CompletePicture(MPEG2DecoderFrame& frame, uint8_t fieldIndex)
     {
         frame.StartDecoding();
@@ -922,7 +948,18 @@ namespace UMC_MPEG2_DECODER
             return UMC::UMC_OK;
         }
 
-        Submit(frame, fieldIndex);
+        size_t sliceCount = frameInfo.GetSliceCount();
+
+        if (sliceCount)
+        {
+            EliminateSliceErrors(frame, fieldIndex);
+            Submit(frame, fieldIndex);
+        }
+        else
+        {
+            if (((info.IsField() && fieldIndex) || !info.IsField()))
+                frame.OnDecodingCompleted();
+        }
 
         return UMC::UMC_OK;
     }

commit e53bc1a7af1aae6a0042df0b66e3e10eeb69d961

[mpeg2d] fix regression in invalid slice handling
Fix a regression introduced in commit 4f328187b7cf
(pull-request #1619) that caused output artifacts
in bottom row of decoded mpeg2 files.

_studio/shared/umc/codec/mpeg2_dec/src/umc_mpeg2_bitstream.cpp
@@ -300,7 +300,7 @@ namespace UMC_MPEG2_DECODER
         uint32_t height_in_MBs = mfx::align2_value<uint32_t>(slice.GetSeqHeader().vertical_size_value, 16) / 16u;
 
         // invalid slice - skipping it
-        if (slice.sliceHeader.slice_vertical_position >= height_in_MBs)
+        if (slice.sliceHeader.slice_vertical_position > height_in_MBs)
             throw mpeg2_exception(UMC::UMC_ERR_INVALID_PARAMS);
 
         slice.sliceHeader.numberMBsInSlice = width_in_MBs - slice.sliceHeader.macroblockAddressIncrement;