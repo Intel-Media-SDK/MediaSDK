Intel-Media-SDK/MediaSDK

commit 5aabcbec6e2c8b8b876631dc05a048c70ccbaa00

[tutorials] Switched to render node initialization by default
The main reason is the render node doesn't require root rights.

Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

tutorials/common/common_vaapi.cpp
@@ -18,9 +18,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "common_vaapi.h"
+#include <sys/ioctl.h>
+#include <drm/drm_fourcc.h>
+#include <map>
 
-#include<map>
+#include "common_vaapi.h"
 
 struct sharedResponse {
     mfxFrameAllocResponse mfxResponse;
@@ -76,7 +78,45 @@ mfxStatus va_to_mfx_status(VAStatus va_res)
 VADisplay m_va_dpy = NULL;
 // gfx card file descriptor
 int m_fd = -1;
-// decoder surfaces chain shared between app and MSDK
+
+constexpr uint32_t DRI_MAX_NODES_NUM = 16;
+constexpr uint32_t DRI_RENDER_START_INDEX = 128;
+constexpr  uint32_t DRM_DRIVER_NAME_LEN = 4;
+const char* DRM_INTEL_DRIVER_NAME = "i915";
+const char* DRI_PATH = "/dev/dri/";
+const char* DRI_NODE_RENDER = "renderD";
+
+int get_drm_driver_name(int fd, char *name, int name_size)
+{
+    drm_version_t version = {};
+    version.name_len = name_size;
+    version.name = name;
+    return ioctl(fd, DRM_IOWR(0, drm_version), &version);
+}
+
+int open_intel_adapter()
+{
+    std::string adapterPath = DRI_PATH;
+    adapterPath += DRI_NODE_RENDER;
+
+    char driverName[DRM_DRIVER_NAME_LEN + 1] = {};
+    mfxU32 nodeIndex = DRI_RENDER_START_INDEX;
+
+    for (mfxU32 i = 0; i < DRI_MAX_NODES_NUM; ++i) {
+        std::string curAdapterPath = adapterPath + std::to_string(nodeIndex + i);
+
+        int fd = open(curAdapterPath.c_str(), O_RDWR);
+        if (fd < 0) continue;
+
+        if (!get_drm_driver_name(fd, driverName, DRM_DRIVER_NAME_LEN) &&
+            !strcmp(driverName, DRM_INTEL_DRIVER_NAME)) {
+            return fd;
+        }
+        close(fd);
+    }
+
+    return -1;
+}
 
 mfxStatus CreateVAEnvDRM(mfxHDL* displayHandle)
 {
@@ -84,7 +124,7 @@ mfxStatus CreateVAEnvDRM(mfxHDL* displayHandle)
     mfxStatus sts = MFX_ERR_NONE;
     int major_version = 0, minor_version = 0;
 
-    m_fd = open("/dev/dri/card0", O_RDWR);
+    m_fd = open_intel_adapter();
 
     if (m_fd < 0)
         sts = MFX_ERR_NOT_INITIALIZED;

commit 57028818165e692db1a9363dd106c84c830b553f

[tutorials] use userspace drm headers
This fixes a compile issue introduced in 5aabcbec6e2c.

Many linux distributions install kernel space drm headers
and user space drm headers in two different locations,
/usr/include/drm and /usr/include/libdrm, respectively.

On Ubuntu Xenial, the linux-libc-dev installs the kernel
space headers and libdrm-dev installs the user space
headers.  Unfortunately, and perhaps erroneously, the
linux-libc-dev drm headers are older than the libdrm-dev
headers.  The older "drm_fourcc.h" header does not include
drm.h for us and thus causes compilation error in tutorials
due to undefined drm_version_t.  The drm.h header was not
included in the drm_fourcc.h header until 2.4.68.

Ensure we are using the user space drm headers via
pkg-config's libdrm include path.  Also, include drm.h
in translation unit for maximum compat.

Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

tutorials/CMakeLists.txt
@@ -18,21 +18,31 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-add_subdirectory(common)
-add_subdirectory(simple_1_session)
-add_subdirectory(simple_2_decode)
-add_subdirectory(simple_2_decode_vmem)
-add_subdirectory(simple_3_encode)
-add_subdirectory(simple_3_encode_vmem)
-add_subdirectory(simple_3_encode_vmem_async)
-add_subdirectory(simple_4_vpp_resize_denoise)
-add_subdirectory(simple_4_vpp_resize_denoise_vmem)
-add_subdirectory(simple_5_transcode)
-add_subdirectory(simple_5_transcode_opaque)
-add_subdirectory(simple_5_transcode_opaque_async)
-add_subdirectory(simple_5_transcode_opaque_async_vppresize)
-add_subdirectory(simple_5_transcode_vmem)
-add_subdirectory(simple_6_decode_vpp_postproc)
-add_subdirectory(simple_6_encode_vmem_lowlatency)
-add_subdirectory(simple_6_encode_vmem_vpp_preproc)
-add_subdirectory(simple_6_transcode_opaque_lowlatency)
+if( PKG_LIBDRM_FOUND )
+
+  set( DEPENDENCIES libdrm )
+
+  add_subdirectory(common)
+  add_subdirectory(simple_1_session)
+  add_subdirectory(simple_2_decode)
+  add_subdirectory(simple_2_decode_vmem)
+  add_subdirectory(simple_3_encode)
+  add_subdirectory(simple_3_encode_vmem)
+  add_subdirectory(simple_3_encode_vmem_async)
+  add_subdirectory(simple_4_vpp_resize_denoise)
+  add_subdirectory(simple_4_vpp_resize_denoise_vmem)
+  add_subdirectory(simple_5_transcode)
+  add_subdirectory(simple_5_transcode_opaque)
+  add_subdirectory(simple_5_transcode_opaque_async)
+  add_subdirectory(simple_5_transcode_opaque_async_vppresize)
+  add_subdirectory(simple_5_transcode_vmem)
+  add_subdirectory(simple_6_decode_vpp_postproc)
+  add_subdirectory(simple_6_encode_vmem_lowlatency)
+  add_subdirectory(simple_6_encode_vmem_vpp_preproc)
+  add_subdirectory(simple_6_transcode_opaque_lowlatency)
+
+else()
+
+  message( STATUS "libdrm was not found, the following will not be built: tutorials." )
+
+endif()

tutorials/common/common_vaapi.cpp
@@ -19,7 +19,8 @@
 // SOFTWARE.
 
 #include <sys/ioctl.h>
-#include <drm/drm_fourcc.h>
+#include <drm.h>
+#include <drm_fourcc.h>
 #include <map>
 
 #include "common_vaapi.h"