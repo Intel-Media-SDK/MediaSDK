Intel-Media-SDK/MediaSDK

commit 4e3c14ad7d88b1b63b532097af73fc324edd563c

samples: add support for render node specifying

samples/sample_common/src/vaapi_utils_drm.cpp
@@ -46,7 +46,7 @@ int get_drm_driver_name(int fd, char *name, int name_size)
     return ioctl(fd, DRM_IOWR(0, drm_version), &version);
 }
 
-int open_intel_adapter(int type)
+int open_first_intel_adapter(int type)
 {
     std::string adapterPath = MFX_DRI_PATH;
     char driverName[MFX_DRM_DRIVER_NAME_LEN + 1] = {};
@@ -82,13 +82,35 @@ int open_intel_adapter(int type)
     return -1;
 }
 
-DRMLibVA::DRMLibVA(int type)
+int open_intel_adapter(const std::string& devicePath, int type)
+{
+    if(devicePath.empty())
+        return open_first_intel_adapter(type);
+
+    int fd = open(devicePath.c_str(), O_RDWR);
+
+    if (fd < 0) {
+        msdk_printf(MSDK_STRING("Failed to open specified device\n"));
+        return -1;
+    }
+
+    char driverName[MFX_DRM_DRIVER_NAME_LEN + 1] = {};
+    if (!get_drm_driver_name(fd, driverName, MFX_DRM_DRIVER_NAME_LEN) &&
+        !strcmp(driverName, MFX_DRM_INTEL_DRIVER_NAME)) {
+            msdk_printf(MSDK_STRING("Specified device is not Intel one\n"));
+            return fd;
+    }
+
+    return -1;
+}
+
+DRMLibVA::DRMLibVA(const std::string& devicePath, int type)
     : CLibVA(type)
     , m_fd(-1)
 {
     mfxStatus sts = MFX_ERR_NONE;
 
-    m_fd = open_intel_adapter(type);
+    m_fd = open_intel_adapter(devicePath, type);
     if (m_fd < 0) throw std::range_error("Intel GPU was not found");
 
     m_va_dpy = m_vadrmlib.vaGetDisplayDRM(m_fd);

samples/sample_common/include/vaapi_device.h
@@ -26,14 +26,14 @@ or https://software.intel.com/en-us/media-client-solutions-support.
 #include "vaapi_utils_android.h"
 #endif
 
-CHWDevice* CreateVAAPIDevice(int type = MFX_LIBVA_DRM);
+CHWDevice* CreateVAAPIDevice(const std::string& devicePath = "", int type = MFX_LIBVA_DRM);
 
 #if defined(LIBVA_DRM_SUPPORT)
 /** VAAPI DRM implementation. */
 class CVAAPIDeviceDRM : public CHWDevice
 {
 public:
-    CVAAPIDeviceDRM(int type);
+    CVAAPIDeviceDRM(const std::string& devicePath, int type);
     virtual ~CVAAPIDeviceDRM(void);
 
     virtual mfxStatus Init(mfxHDL hWindow, mfxU16 nViews, mfxU32 nAdapterNum);

samples/sample_common/include/vaapi_utils.h
@@ -465,7 +465,7 @@ private:
     DISALLOW_COPY_AND_ASSIGN(CLibVA);
 };
 
-CLibVA* CreateLibVA(int type = MFX_LIBVA_DRM);
+CLibVA* CreateLibVA(const std::string& devicePath = "", int type = MFX_LIBVA_DRM);
 
 VAStatus AcquireVASurface(void** ctx, VADisplay dpy1, VASurfaceID srf1, VADisplay dpy2, VASurfaceID* srf2);
 void ReleaseVASurface(void* actx, VADisplay dpy1, VASurfaceID srf1, VADisplay dpy2, VASurfaceID srf2);

samples/sample_common/include/vaapi_utils_drm.h
@@ -32,7 +32,7 @@ class drmRenderer;
 class DRMLibVA : public CLibVA
 {
 public:
-    DRMLibVA(int type = MFX_LIBVA_DRM);
+    DRMLibVA(const std::string& devicePath = "", int type = MFX_LIBVA_DRM);
     virtual ~DRMLibVA(void);
 
     inline int getFD() { return m_fd; }

samples/sample_common/src/vaapi_device.cpp
@@ -416,8 +416,8 @@ CHWDevice* CreateVAAPIDevice(void)
 
 #if defined(LIBVA_DRM_SUPPORT)
 
-CVAAPIDeviceDRM::CVAAPIDeviceDRM(int type)
-    : m_DRMLibVA(type)
+CVAAPIDeviceDRM::CVAAPIDeviceDRM(const std::string& devicePath, int type)
+    : m_DRMLibVA(devicePath, type)
     , m_rndr(NULL)
 {
 }
@@ -458,7 +458,7 @@ mfxStatus CVAAPIDeviceDRM::RenderFrame(mfxFrameSurface1 * pSurface, mfxFrameAllo
 
 #if defined(LIBVA_DRM_SUPPORT) || defined(LIBVA_X11_SUPPORT) || defined (LIBVA_WAYLAND_SUPPORT)
 
-CHWDevice* CreateVAAPIDevice(int type)
+CHWDevice* CreateVAAPIDevice(const std::string& devicePath, int type)
 {
     CHWDevice * device = NULL;
 
@@ -469,7 +469,7 @@ CHWDevice* CreateVAAPIDevice(int type)
 #if defined(LIBVA_DRM_SUPPORT)
         try
         {
-            device = new CVAAPIDeviceDRM(type);
+            device = new CVAAPIDeviceDRM(devicePath, type);
         }
         catch (std::exception&)
         {
@@ -511,7 +511,7 @@ CHWDevice* CreateVAAPIDevice(int type)
         {
             try
             {
-                device = new CVAAPIDeviceDRM(type);
+                device = new CVAAPIDeviceDRM(devicePath, type);
             }
             catch (std::exception&)
             {

samples/sample_common/src/vaapi_utils.cpp
@@ -288,7 +288,7 @@ mfxStatus va_to_mfx_status(VAStatus va_res)
 }
 
 #if defined(LIBVA_DRM_SUPPORT) || defined(LIBVA_X11_SUPPORT)
-CLibVA* CreateLibVA(int type)
+CLibVA* CreateLibVA(const std::string& devicePath, int type)
 {
     CLibVA * libva = NULL;
     switch (type)
@@ -297,7 +297,7 @@ CLibVA* CreateLibVA(int type)
 #if defined(LIBVA_DRM_SUPPORT)
         try
         {
-            libva = new DRMLibVA(type);
+            libva = new DRMLibVA(devicePath, type);
         }
         catch (std::exception&)
         {
@@ -335,7 +335,7 @@ CLibVA* CreateLibVA(int type)
         {
             try
             {
-                libva = new DRMLibVA(type);
+                libva = new DRMLibVA(devicePath, type);
             }
             catch (std::exception&)
             {

samples/sample_decode/src/pipeline_decode.cpp
@@ -1054,7 +1054,7 @@ mfxStatus CDecodingPipeline::CreateHWDevice()
         m_d3dRender.SetHWDevice(m_hwdev);
 #elif LIBVA_SUPPORT
     mfxStatus sts = MFX_ERR_NONE;
-    m_hwdev = CreateVAAPIDevice(m_libvaBackend);
+    m_hwdev = CreateVAAPIDevice("", m_libvaBackend);
 
     if (NULL == m_hwdev) {
         return MFX_ERR_MEMORY_ALLOC;

samples/sample_multi_transcode/src/sample_multi_transcode.cpp
@@ -213,7 +213,7 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
             libvaBackend = params.libvaBackend;
 
             /* Rendering case */
-            m_hwdev.reset(CreateVAAPIDevice(params.libvaBackend));
+            m_hwdev.reset(CreateVAAPIDevice("", params.libvaBackend));
             if(!m_hwdev.get()) {
                 msdk_printf(MSDK_STRING("error: failed to initialize VAAPI device\n"));
                 return MFX_ERR_DEVICE_FAILED;
@@ -678,7 +678,7 @@ mfxStatus Launcher::ProcessResult()
            << MSDK_STRING("] ") << SessionStsStr <<MSDK_STRING(" (")
            << StatusToString(transcodingSts) << MSDK_STRING(") ")
            << workTime << MSDK_STRING(" sec, ")
-           << framesNum << MSDK_STRING(" frames, ") 
+           << framesNum << MSDK_STRING(" frames, ")
            << std::fixed << std::setprecision(3) << framesNum / workTime << MSDK_STRING(" fps")
            << std::endl
            << m_parser.GetLine(i) << std::endl << std::endl;

commit 660c753a8ef9e6b7fecf20cb8222cc3a141e7dc4

Samples: Fix behavior when specified non-intel device
Fixes: 4e3c14a ("samples: add support for render node specifying")

samples/sample_common/src/vaapi_utils_drm.cpp
@@ -97,11 +97,13 @@ int open_intel_adapter(const std::string& devicePath, int type)
     char driverName[MFX_DRM_DRIVER_NAME_LEN + 1] = {};
     if (!get_drm_driver_name(fd, driverName, MFX_DRM_DRIVER_NAME_LEN) &&
         !strcmp(driverName, MFX_DRM_INTEL_DRIVER_NAME)) {
-            msdk_printf(MSDK_STRING("Specified device is not Intel one\n"));
             return fd;
     }
-
-    return -1;
+    else {
+        close(fd);
+        msdk_printf(MSDK_STRING("Specified device is not Intel one\n"));
+        return -1;
+    }
 }
 
 DRMLibVA::DRMLibVA(const std::string& devicePath, int type)