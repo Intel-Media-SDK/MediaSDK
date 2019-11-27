Intel-Media-SDK/MediaSDK

commit 444ecb480a3918cc6faeb7662ca7492fd564f301

Add 12-bit format support

_studio/shared/umc/io/umc_va/src/umc_va_linux.cpp
@@ -97,6 +97,13 @@ VAEntrypoint umc_to_va_entrypoint(uint32_t umc_entrypoint)
     case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_444:
     case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_10 | UMC::VA_PROFILE_422:
     case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_10 | UMC::VA_PROFILE_444:
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    case UMC::VA_VLD |                        UMC::VA_PROFILE_12:
+    case UMC::VA_VLD |                        UMC::VA_PROFILE_12 | UMC::VA_PROFILE_444:
+    case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12:
+    case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_422:
+    case UMC::VA_VLD | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_444:
+#endif
         va_entrypoint = VAEntrypointVLD;
         break;
     default:
@@ -202,6 +209,16 @@ VAProfile get_next_va_profile(uint32_t umc_codec, uint32_t profile)
         if (profile < 1) va_profile = VAProfileHEVCMain444_10;
         break;
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12:
+    case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_422:
+        va_profile = VAProfileHEVCMain422_12;
+        break;
+    case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_444:
+        va_profile = VAProfileHEVCMain444_12;
+        break;
+#endif
+
     case UMC::VA_VC1:
         if (profile < UMC_ARRAY_SIZE(g_VC1Profiles)) va_profile = g_VC1Profiles[profile];
         break;
@@ -216,6 +233,13 @@ VAProfile get_next_va_profile(uint32_t umc_codec, uint32_t profile)
     case UMC::VA_VP9 | UMC::VA_PROFILE_444 | UMC::VA_PROFILE_10:
         if (profile < UMC_ARRAY_SIZE(g_VP910BitsProfiles)) va_profile = g_VP910BitsProfiles[profile];
         break;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    case UMC::VA_VP9 | UMC::VA_PROFILE_12:
+    case UMC::VA_VP9 | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_444:
+        if (profile < UMC_ARRAY_SIZE(g_VP910BitsProfiles)) va_profile = g_VP910BitsProfiles[profile];
+        break;
+
+#endif
     case UMC::VA_JPEG:
         if (profile < UMC_ARRAY_SIZE(g_JPEGProfiles)) va_profile = g_JPEGProfiles[profile];
         break;

_studio/mfx_lib/decode/h265/src/mfx_h265_dec_decode.cpp
@@ -118,6 +118,11 @@ UMC::Status FillParam(VideoCORE *core, MFXTaskSupplier_H265 * decoder, mfxVideoP
 #if (MFX_VERSION >= 1027)
             || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y216
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
+#endif
             )
             par->mfx.FrameInfo.Shift = 1;
     }
@@ -291,6 +296,11 @@ mfxStatus VideoDECODEH265::Init(mfxVideoParam *par)
             if (   par->mfx.FrameInfo.FourCC == MFX_FOURCC_P010
     #if (MFX_VERSION >= 1027)
                 || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210
+    #endif
+    #if (MFX_VERSION >= MFX_VERSION_NEXT)
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y216
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
     #endif
                 )
 

_studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode.cpp
@@ -242,6 +242,12 @@ void FillVideoParam(eMFXPlatform platform, UMC_VP9_DECODER::VP9DecoderFrame cons
 
         case 12:
             params.mfx.FrameInfo.FourCC = 0;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            if (MFX_CHROMAFORMAT_YUV420 == params.mfx.FrameInfo.ChromaFormat)
+                params.mfx.FrameInfo.FourCC = MFX_FOURCC_P016;
+            else if(MFX_CHROMAFORMAT_YUV444 == params.mfx.FrameInfo.ChromaFormat)
+                params.mfx.FrameInfo.FourCC = MFX_FOURCC_Y416;
+#endif
             params.mfx.FrameInfo.BitDepthLuma   = 12;
             params.mfx.FrameInfo.BitDepthChroma = 12;
             break;
@@ -251,7 +257,12 @@ void FillVideoParam(eMFXPlatform platform, UMC_VP9_DECODER::VP9DecoderFrame cons
     {
         params.mfx.FrameInfo.Shift = 0;
 
-        if (params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010)
+        if (params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            || params.mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+            || params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
+#endif
+        )
         {
             params.mfx.FrameInfo.Shift = 1;
         }

_studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
@@ -915,6 +915,10 @@ static mfxStatus CheckFrameInfo(mfxFrameInfo const &currInfo, mfxFrameInfo &info
         case MFX_FOURCC_Y410:
 #endif
             break;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        case MFX_FOURCC_P016:
+        case MFX_FOURCC_Y416:
+#endif
         case MFX_FOURCC_P010:
 #if (MFX_VERSION >= 1027)
         case MFX_FOURCC_Y210:

_studio/mfx_lib/encode_hw/h265/include/mfx_h265_encode_hw_ddi.h
@@ -67,6 +67,12 @@ static const GUID DXVA2_Intel_LowpowerEncode_HEVC_Main444 =
 static const GUID DXVA2_Intel_LowpowerEncode_HEVC_Main444_10 =
 { 0x10e19ac8, 0xbf39, 0x4443, { 0xbe, 0xc3, 0x1b, 0x0c, 0xbf, 0xe4, 0xc7, 0xaa } };
 
+static const GUID DXVA2_Intel_Encode_HEVC_Main12 =
+{ 0xd6d6bc4f, 0xd51a, 0x4712, { 0x97, 0xe8, 0x75, 0x9, 0x17, 0xc8, 0x60, 0xfd } };
+static const GUID DXVA2_Intel_Encode_HEVC_Main422_12 =
+{ 0x7fef652d, 0x3233, 0x44df, { 0xac, 0xf7, 0xec, 0xfb, 0x58, 0x4d, 0xab, 0x35 } };
+static const GUID DXVA2_Intel_Encode_HEVC_Main444_12 =
+{ 0xf8fa34b7, 0x93f5, 0x45a4, { 0xbf, 0xc0, 0x38, 0x17, 0xce, 0xe6, 0xbb, 0x93 } };
 GUID GetGUID(MfxVideoParam const & par);
 
 const GUID GuidTable[3][3][3] =
@@ -87,6 +93,11 @@ const GUID GuidTable[3][3][3] =
         },
         // BitDepthLuma = 12
         {
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            /*420*/ DXVA2_Intel_Encode_HEVC_Main12,
+            /*422*/ DXVA2_Intel_Encode_HEVC_Main422_12,
+            /*444*/ DXVA2_Intel_Encode_HEVC_Main444_12
+#endif
         }
     },
     // LowPower = ON
@@ -108,7 +119,7 @@ const GUID GuidTable[3][3][3] =
         }
     }
 };
-mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* pCore);
+mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* pCore, MfxVideoParam const &par);
 
 class DriverEncoder;
 

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw.cpp
@@ -126,9 +126,20 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 10)
     {
-        rec.FourCC = MFX_FOURCC_Y210;
-        rec.Width /= 2;
-        rec.Height *= 2;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        if (par.m_platform >= MFX_HW_TGL_LP)
+        {
+            rec.FourCC = MFX_FOURCC_Y216;
+            rec.Width /= 2;
+            rec.Height *= 2;
+        }
+        else
+#endif
+        {
+            rec.FourCC = MFX_FOURCC_Y210;
+            rec.Width /= 2;
+            rec.Height *= 2;
+        }
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 8)
     {
@@ -138,13 +149,41 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 10)
     {
-        rec.FourCC = MFX_FOURCC_NV12;
-        rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        if (par.m_platform >= MFX_HW_TGL_LP)
+        {
+            rec.FourCC = MFX_FOURCC_NV12;
+            rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+        }
+        else
+#endif
+        {
+            rec.FourCC = MFX_FOURCC_P010;
+        }
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 8)
     {
         rec.FourCC = MFX_FOURCC_NV12;
     }
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV444) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_Y416;
+        rec.Width = (mfxU16)mfx::align2_value(rec.Width, 256 / 4);
+        rec.Height = (mfxU16)mfx::align2_value(rec.Height * 3 / 2, 8);
+    }
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_Y216;
+        rec.Width /= 2;
+        rec.Height *= 2;
+    }
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_NV12;
+        rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+    }
+#endif
     else
     {
         assert(!"undefined target format");
@@ -163,6 +202,7 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     Setting default value for LowPower option.
     By default LowPower is OFF (using DualPipe)
     For CNL: if no B-frames found and LowPower is Unknown then LowPower is ON
+    For JSL/EHL: use LowPower by default i.e. if LowPower is Unknown then LowPower is ON
 
     Return value:
     MFX_WRN_INCOMPATIBLE_VIDEO_PARAM - if initial value of par.mfx.LowPower is not equal to MFX_CODINGOPTION_ON, MFX_CODINGOPTION_OFF or MFX_CODINGOPTION_UNKNOWN
@@ -182,6 +222,14 @@ mfxStatus SetLowpowerDefault(MfxVideoParam& par)
         return sts;
     }
 #endif // MFX_VERSION >= 1025
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    if ((par.m_platform == MFX_HW_EHL)
+        && par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
+    {
+        par.mfx.LowPower = MFX_CODINGOPTION_ON;
+        return sts;
+    }
+#endif
     if (par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
         par.mfx.LowPower = MFX_CODINGOPTION_OFF;
 
@@ -432,6 +480,11 @@ mfxStatus MFXVideoENCODEH265_HW::QueryIOSurf(VideoCORE *core, mfxVideoParam *par
     request->Info = tmp.mfx.FrameInfo;
 #if (MFX_VERSION >= 1027)
     request->Info.Shift = (tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_P010 ||
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_P016 ||
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y216 ||
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y416 ||
+#endif
                            tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y210) ? 1: 0;
 #else
     request->Info.Shift = tmp.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10? 1: 0;

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_ddi.cpp
@@ -40,9 +40,13 @@ GUID GetGUID(MfxVideoParam const & par)
 #if (MFX_VERSION >= 1027)
     if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 || par.m_ext.CO3.TargetBitDepthLuma == 10)
         bdId = 1;
+    if (par.m_ext.CO3.TargetBitDepthLuma == 12)
+        bdId = 2;
 
     cfId = mfx::clamp<mfxU16>(par.m_ext.CO3.TargetChromaFormatPlus1 - 1, MFX_CHROMAFORMAT_YUV420, MFX_CHROMAFORMAT_YUV444) - MFX_CHROMAFORMAT_YUV420;
 
+    if (par.m_platform && par.m_platform < MFX_HW_ICL)
+        cfId = 0; // platforms below ICL do not support Main422/Main444 profile, using Main instead.
 #else
     if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 || par.mfx.FrameInfo.BitDepthLuma == 10 || par.mfx.FrameInfo.FourCC == MFX_FOURCC_P010)
         bdId = 1;
@@ -80,7 +84,7 @@ DriverEncoder* CreatePlatformH265Encoder(VideoCORE* core, ENCODER_TYPE type)
 }
 
 // this function is aimed to workaround all CAPS reporting problems in mainline driver
-mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
+mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core, MfxVideoParam const &par)
 {
     mfxStatus sts = MFX_ERR_NONE;
     MFX_CHECK_NULL_PTR1(core);
@@ -103,6 +107,8 @@ mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
         caps.ddi_caps.NegativeQPSupport = 0;
     else
         caps.ddi_caps.NegativeQPSupport = 1; // driver should set it for Gen11+ VME only
+		
+    caps.PSliceSupport = (IsOn(par.mfx.LowPower) || platform > MFX_HW_ICL) ? 0 : 1;
 
 #if defined(MFX_ENABLE_HEVCE_WEIGHTED_PREDICTION)
     if (platform >= MFX_HW_ICL)
@@ -125,6 +131,36 @@ mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
     if (!caps.ddi_caps.LCUSizeSupported)
         caps.ddi_caps.LCUSizeSupported = 2;
     caps.ddi_caps.BlockSize = 2; // 32x32
+    // align with Windows for Gen12+
+    if (platform >= MFX_HW_TGL_LP)
+    {   // taken from  Windows TGLLP (temporarily)
+        caps.ddi_caps.CodingLimitSet = 1; // = 0 now but should be always set to 1 according to DDI (what about Linux ???)
+        caps.ddi_caps.Color420Only = 0;  // = 1 now
+        caps.ddi_caps.YUV422ReconSupport = 1; // = 0 now
+        caps.ddi_caps.SliceIPBOnly = 1;  // = 0 now (SliceIP is also 0)cz
+        caps.ddi_caps.NoWeightedPred = 0; // = 1 now
+        caps.ddi_caps.NoMinorMVs = 1;  // = 0 now
+        caps.ddi_caps.RawReconRefToggle = 1;  // = 0 now
+        caps.ddi_caps.NoInterlacedField = 1;  // = 0 now
+//        caps.RollingIntraRefresh = 0;  // = 1 now
+//        caps.VCMBitRateControl = 0;  // = 1 now
+        caps.ddi_caps.ParallelBRC = 1;  // = 0 now
+        caps.ddi_caps.LumaWeightedPred = 1; // = 0 now
+        caps.ddi_caps.ChromaWeightedPred = 0; // = 0 now
+        caps.ddi_caps.MaxEncodedBitDepth = 2;  // = 1 now (8/10b only)
+        caps.ddi_caps.MaxPicWidth = 16384;  // = 8192 now
+        caps.ddi_caps.MaxPicHeight = 16384;  // = 8192 now
+        caps.ddi_caps.MaxNumOfROI = 16;  // = 0 now
+        caps.ddi_caps.ROIDeltaQPSupport = 1;  // = 0 now
+        caps.ddi_caps.BlockSize = 1;  // = 0 now (set to 16x16 like in Win)
+        caps.ddi_caps.SliceLevelReportSupport = 1;  // = 0 now
+        caps.ddi_caps.FrameSizeToleranceSupport = 1;  // = 0 now
+        caps.ddi_caps.NumScalablePipesMinus1 = 1;  // = 0 now
+        caps.ddi_caps.MaxNum_WeightedPredL0 = 4; // = 0 now
+        caps.ddi_caps.MaxNum_WeightedPredL1 = 2; // = 0 now
+        caps.ddi_caps.TileSupport = 1;
+        caps.ddi_caps.IntraRefreshBlockUnitSize = 2;
+    }
     (void)core;
 #endif
 

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -521,6 +521,15 @@ mfxU16 MakeSlices(MfxVideoParam& par, mfxU32 SliceStructure)
     if (nTile == 1) //TileScan = RasterScan, no SegmentAddress conversion required
         return (mfxU16)AddTileSlices(par, SliceStructure, nCol, nRow, nSlice);
 
+    if (nTile > 1 && nSlice > nTile && IsOn(par.mfx.LowPower))
+    {
+        // DDI:0.9972: VDEnc supports multiple static slices in case of single tile frame only.
+        //             But for multi tiles frame, no single tile can contain multiple static slices.
+        // Actually it's no matter for driver whether dynamic or static slices are used
+        // It returns an error in the both cases.
+        nSlice = nTile;
+    }
+
     std::vector<mfxU32> colWidth(nTCol, 0);
     std::vector<mfxU32> rowHeight(nTRow, 0);
     std::vector<mfxU32> colBd(nTCol+1, 0);
@@ -956,7 +965,8 @@ mfxStatus CheckAndFixRoi(MfxVideoParam  const & par, ENCODE_CAPS_HEVC const & ca
 
     if (!(par.mfx.RateControlMethod == MFX_RATECONTROL_CBR ||
         par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-        par.mfx.RateControlMethod == MFX_RATECONTROL_CQP))
+        par.mfx.RateControlMethod == MFX_RATECONTROL_CQP ||
+        par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR))
     {
         invalid++;
     }

commit 11c4ad6e7ecad1161f1f971840a00f2245bfe129

Fix finding supported profile for 12-bit color formats
In conjunciton with 48d764d ("[va] Fix finding supported profile")

Fixes: 444ecb4 ("Add 12-bit format support")

Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

_studio/shared/umc/io/umc_va/src/umc_va_linux.cpp
@@ -212,10 +212,10 @@ VAProfile get_next_va_profile(uint32_t umc_codec, uint32_t profile)
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
     case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12:
     case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_422:
-        va_profile = VAProfileHEVCMain422_12;
+        if (profile < 1) va_profile = VAProfileHEVCMain422_12;
         break;
     case UMC::VA_H265 | UMC::VA_PROFILE_REXT | UMC::VA_PROFILE_12 | UMC::VA_PROFILE_444:
-        va_profile = VAProfileHEVCMain444_12;
+        if (profile < 1) va_profile = VAProfileHEVCMain444_12;
         break;
 #endif
 