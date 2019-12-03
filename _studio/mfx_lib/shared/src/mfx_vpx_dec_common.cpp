Intel-Media-SDK/MediaSDK

commit 444ecb480a3918cc6faeb7662ca7492fd564f301

Add 12-bit format support

_studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
@@ -109,6 +109,10 @@ namespace MFX_VPX_Utility
 #if (MFX_VERSION >= 1027)
             case MFX_FOURCC_Y410:
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            case MFX_FOURCC_P016:
+            case MFX_FOURCC_Y416:
+#endif
                 p_out->mfx.FrameInfo.FourCC = p_in->mfx.FrameInfo.FourCC;
                 break;
             default:
@@ -138,6 +142,10 @@ namespace MFX_VPX_Utility
                   //|| (p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210 && p_in->mfx.FrameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV422)
                     || (p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_Y410 && p_in->mfx.FrameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV444)
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+                    || (p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_P016 && p_in->mfx.FrameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV420)
+                    || (p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416 && p_in->mfx.FrameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV444)
+#endif
                     )
                 {
                     p_out->mfx.FrameInfo.FourCC = 0;
@@ -150,13 +158,12 @@ namespace MFX_VPX_Utility
 
             p_out->mfx.FrameInfo.BitDepthLuma   = p_in->mfx.FrameInfo.BitDepthLuma;
             p_out->mfx.FrameInfo.BitDepthChroma = p_in->mfx.FrameInfo.BitDepthChroma;
-            p_out->mfx.FrameInfo.Shift          = p_in->mfx.FrameInfo.Shift;
 
-            if ((p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_NV12 ||
-                 p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_AYUV) &&
-                 ((p_in->mfx.FrameInfo.BitDepthLuma   != 0 && p_in->mfx.FrameInfo.BitDepthLuma   != 8) ||
-                  (p_in->mfx.FrameInfo.BitDepthChroma != 0 && p_in->mfx.FrameInfo.BitDepthChroma != 8) ||
-                  p_in->mfx.FrameInfo.Shift))
+            if ((p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_NV12
+                || p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_AYUV) &&
+               ((p_in->mfx.FrameInfo.BitDepthLuma   != 0 && p_in->mfx.FrameInfo.BitDepthLuma   != 8) ||
+                (p_in->mfx.FrameInfo.BitDepthChroma != 0 && p_in->mfx.FrameInfo.BitDepthChroma != 8) ||
+                    p_in->mfx.FrameInfo.Shift))
             {
                 p_out->mfx.FrameInfo.BitDepthLuma = 0;
                 p_out->mfx.FrameInfo.BitDepthChroma = 0;
@@ -176,16 +183,28 @@ namespace MFX_VPX_Utility
                 p_out->mfx.FrameInfo.Shift = 0;
                 sts = MFX_ERR_UNSUPPORTED;
             }
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            if ((  p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+                || p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416) &&
+                ((p_in->mfx.FrameInfo.BitDepthLuma != 0 && p_in->mfx.FrameInfo.BitDepthLuma != 12) ||
+                 (p_in->mfx.FrameInfo.BitDepthChroma != 0 && p_in->mfx.FrameInfo.BitDepthChroma != 12)))
+            {
+                p_out->mfx.FrameInfo.BitDepthLuma = 0;
+                p_out->mfx.FrameInfo.BitDepthChroma = 0;
+                p_out->mfx.FrameInfo.Shift = 0;
+                sts = MFX_ERR_UNSUPPORTED;
+            }
+#endif
 
             if (!p_in->mfx.FrameInfo.ChromaFormat && !(!p_in->mfx.FrameInfo.FourCC && !p_in->mfx.FrameInfo.ChromaFormat))
                 sts = MFX_ERR_UNSUPPORTED;
 
-            if (p_in->mfx.FrameInfo.Width % 16 == 0)
+            if (p_in->mfx.FrameInfo.Width % 16 == 0 && p_in->mfx.FrameInfo.Width <= GetMaxWidth(codecId))
                 p_out->mfx.FrameInfo.Width = p_in->mfx.FrameInfo.Width;
             else
                 sts = MFX_ERR_UNSUPPORTED;
 
-            if (p_in->mfx.FrameInfo.Height % 16 == 0)
+            if (p_in->mfx.FrameInfo.Height % 16 == 0 && p_in->mfx.FrameInfo.Height <= GetMaxHeight(codecId))
                 p_out->mfx.FrameInfo.Height = p_in->mfx.FrameInfo.Height;
             else
                 sts = MFX_ERR_UNSUPPORTED;
@@ -326,6 +345,11 @@ namespace MFX_VPX_Utility
                 //&& p_in->mfx.FrameInfo.FourCC != MFX_FOURCC_Y210
                 && !(frameInfo.FourCC == MFX_FOURCC_Y410 && hwtype >= MFX_HW_ICL)
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+                && frameInfo.FourCC != MFX_FOURCC_P016
+                && frameInfo.FourCC != MFX_FOURCC_Y416
+#endif
+
             )
                 return false;
 
@@ -347,6 +371,10 @@ namespace MFX_VPX_Utility
                     || (frameInfo.FourCC == MFX_FOURCC_P010 && frameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV420)
 #if (MFX_VERSION >= 1027)
                     || (frameInfo.FourCC == MFX_FOURCC_Y410 && frameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV444)
+#endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+                   || (frameInfo.FourCC == MFX_FOURCC_P016 && frameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV420)
+                   || (frameInfo.FourCC == MFX_FOURCC_Y416 && frameInfo.ChromaFormat != MFX_CHROMAFORMAT_YUV444)
 #endif
                     )
                     return false;

_studio/mfx_lib/decode/h265/src/mfx_h265_dec_decode.cpp
@@ -118,6 +118,11 @@ UMC::Status FillParam(VideoCORE *core, MFXTaskSupplier_H265 * decoder, mfxVideoP
 #if (MFX_VERSION >= 1027)
             || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210
 #endif
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y216
+            || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
+#endif
             )
             par->mfx.FrameInfo.Shift = 1;
     }
@@ -291,6 +296,11 @@ mfxStatus VideoDECODEH265::Init(mfxVideoParam *par)
             if (   par->mfx.FrameInfo.FourCC == MFX_FOURCC_P010
     #if (MFX_VERSION >= 1027)
                 || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210
+    #endif
+    #if (MFX_VERSION >= MFX_VERSION_NEXT)
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y216
+                || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
     #endif
                 )
 

_studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode.cpp
@@ -242,6 +242,12 @@ void FillVideoParam(eMFXPlatform platform, UMC_VP9_DECODER::VP9DecoderFrame cons
 
         case 12:
             params.mfx.FrameInfo.FourCC = 0;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            if (MFX_CHROMAFORMAT_YUV420 == params.mfx.FrameInfo.ChromaFormat)
+                params.mfx.FrameInfo.FourCC = MFX_FOURCC_P016;
+            else if(MFX_CHROMAFORMAT_YUV444 == params.mfx.FrameInfo.ChromaFormat)
+                params.mfx.FrameInfo.FourCC = MFX_FOURCC_Y416;
+#endif
             params.mfx.FrameInfo.BitDepthLuma   = 12;
             params.mfx.FrameInfo.BitDepthChroma = 12;
             break;
@@ -251,7 +257,12 @@ void FillVideoParam(eMFXPlatform platform, UMC_VP9_DECODER::VP9DecoderFrame cons
     {
         params.mfx.FrameInfo.Shift = 0;
 
-        if (params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010)
+        if (params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            || params.mfx.FrameInfo.FourCC == MFX_FOURCC_P016
+            || params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
+#endif
+        )
         {
             params.mfx.FrameInfo.Shift = 1;
         }

_studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
@@ -915,6 +915,10 @@ static mfxStatus CheckFrameInfo(mfxFrameInfo const &currInfo, mfxFrameInfo &info
         case MFX_FOURCC_Y410:
 #endif
             break;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        case MFX_FOURCC_P016:
+        case MFX_FOURCC_Y416:
+#endif
         case MFX_FOURCC_P010:
 #if (MFX_VERSION >= 1027)
         case MFX_FOURCC_Y210:

_studio/mfx_lib/encode_hw/h265/include/mfx_h265_encode_hw_ddi.h
@@ -67,6 +67,12 @@ static const GUID DXVA2_Intel_LowpowerEncode_HEVC_Main444 =
 static const GUID DXVA2_Intel_LowpowerEncode_HEVC_Main444_10 =
 { 0x10e19ac8, 0xbf39, 0x4443, { 0xbe, 0xc3, 0x1b, 0x0c, 0xbf, 0xe4, 0xc7, 0xaa } };
 
+static const GUID DXVA2_Intel_Encode_HEVC_Main12 =
+{ 0xd6d6bc4f, 0xd51a, 0x4712, { 0x97, 0xe8, 0x75, 0x9, 0x17, 0xc8, 0x60, 0xfd } };
+static const GUID DXVA2_Intel_Encode_HEVC_Main422_12 =
+{ 0x7fef652d, 0x3233, 0x44df, { 0xac, 0xf7, 0xec, 0xfb, 0x58, 0x4d, 0xab, 0x35 } };
+static const GUID DXVA2_Intel_Encode_HEVC_Main444_12 =
+{ 0xf8fa34b7, 0x93f5, 0x45a4, { 0xbf, 0xc0, 0x38, 0x17, 0xce, 0xe6, 0xbb, 0x93 } };
 GUID GetGUID(MfxVideoParam const & par);
 
 const GUID GuidTable[3][3][3] =
@@ -87,6 +93,11 @@ const GUID GuidTable[3][3][3] =
         },
         // BitDepthLuma = 12
         {
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+            /*420*/ DXVA2_Intel_Encode_HEVC_Main12,
+            /*422*/ DXVA2_Intel_Encode_HEVC_Main422_12,
+            /*444*/ DXVA2_Intel_Encode_HEVC_Main444_12
+#endif
         }
     },
     // LowPower = ON
@@ -108,7 +119,7 @@ const GUID GuidTable[3][3][3] =
         }
     }
 };
-mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* pCore);
+mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* pCore, MfxVideoParam const &par);
 
 class DriverEncoder;
 

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw.cpp
@@ -126,9 +126,20 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 10)
     {
-        rec.FourCC = MFX_FOURCC_Y210;
-        rec.Width /= 2;
-        rec.Height *= 2;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        if (par.m_platform >= MFX_HW_TGL_LP)
+        {
+            rec.FourCC = MFX_FOURCC_Y216;
+            rec.Width /= 2;
+            rec.Height *= 2;
+        }
+        else
+#endif
+        {
+            rec.FourCC = MFX_FOURCC_Y210;
+            rec.Width /= 2;
+            rec.Height *= 2;
+        }
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 8)
     {
@@ -138,13 +149,41 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 10)
     {
-        rec.FourCC = MFX_FOURCC_NV12;
-        rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+        if (par.m_platform >= MFX_HW_TGL_LP)
+        {
+            rec.FourCC = MFX_FOURCC_NV12;
+            rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+        }
+        else
+#endif
+        {
+            rec.FourCC = MFX_FOURCC_P010;
+        }
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 8)
     {
         rec.FourCC = MFX_FOURCC_NV12;
     }
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV444) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_Y416;
+        rec.Width = (mfxU16)mfx::align2_value(rec.Width, 256 / 4);
+        rec.Height = (mfxU16)mfx::align2_value(rec.Height * 3 / 2, 8);
+    }
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV422) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_Y216;
+        rec.Width /= 2;
+        rec.Height *= 2;
+    }
+    else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 12)
+    {
+        rec.FourCC = MFX_FOURCC_NV12;
+        rec.Width  = mfx::align2_value(rec.Width, 32) * 2;
+    }
+#endif
     else
     {
         assert(!"undefined target format");
@@ -163,6 +202,7 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     Setting default value for LowPower option.
     By default LowPower is OFF (using DualPipe)
     For CNL: if no B-frames found and LowPower is Unknown then LowPower is ON
+    For JSL/EHL: use LowPower by default i.e. if LowPower is Unknown then LowPower is ON
 
     Return value:
     MFX_WRN_INCOMPATIBLE_VIDEO_PARAM - if initial value of par.mfx.LowPower is not equal to MFX_CODINGOPTION_ON, MFX_CODINGOPTION_OFF or MFX_CODINGOPTION_UNKNOWN
@@ -182,6 +222,14 @@ mfxStatus SetLowpowerDefault(MfxVideoParam& par)
         return sts;
     }
 #endif // MFX_VERSION >= 1025
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+    if ((par.m_platform == MFX_HW_EHL)
+        && par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
+    {
+        par.mfx.LowPower = MFX_CODINGOPTION_ON;
+        return sts;
+    }
+#endif
     if (par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
         par.mfx.LowPower = MFX_CODINGOPTION_OFF;
 
@@ -432,6 +480,11 @@ mfxStatus MFXVideoENCODEH265_HW::QueryIOSurf(VideoCORE *core, mfxVideoParam *par
     request->Info = tmp.mfx.FrameInfo;
 #if (MFX_VERSION >= 1027)
     request->Info.Shift = (tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_P010 ||
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_P016 ||
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y216 ||
+                           tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y416 ||
+#endif
                            tmp.mfx.FrameInfo.FourCC == MFX_FOURCC_Y210) ? 1: 0;
 #else
     request->Info.Shift = tmp.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10? 1: 0;

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_ddi.cpp
@@ -40,9 +40,13 @@ GUID GetGUID(MfxVideoParam const & par)
 #if (MFX_VERSION >= 1027)
     if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 || par.m_ext.CO3.TargetBitDepthLuma == 10)
         bdId = 1;
+    if (par.m_ext.CO3.TargetBitDepthLuma == 12)
+        bdId = 2;
 
     cfId = mfx::clamp<mfxU16>(par.m_ext.CO3.TargetChromaFormatPlus1 - 1, MFX_CHROMAFORMAT_YUV420, MFX_CHROMAFORMAT_YUV444) - MFX_CHROMAFORMAT_YUV420;
 
+    if (par.m_platform && par.m_platform < MFX_HW_ICL)
+        cfId = 0; // platforms below ICL do not support Main422/Main444 profile, using Main instead.
 #else
     if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 || par.mfx.FrameInfo.BitDepthLuma == 10 || par.mfx.FrameInfo.FourCC == MFX_FOURCC_P010)
         bdId = 1;
@@ -80,7 +84,7 @@ DriverEncoder* CreatePlatformH265Encoder(VideoCORE* core, ENCODER_TYPE type)
 }
 
 // this function is aimed to workaround all CAPS reporting problems in mainline driver
-mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
+mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core, MfxVideoParam const &par)
 {
     mfxStatus sts = MFX_ERR_NONE;
     MFX_CHECK_NULL_PTR1(core);
@@ -103,6 +107,8 @@ mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
         caps.ddi_caps.NegativeQPSupport = 0;
     else
         caps.ddi_caps.NegativeQPSupport = 1; // driver should set it for Gen11+ VME only
+		
+    caps.PSliceSupport = (IsOn(par.mfx.LowPower) || platform > MFX_HW_ICL) ? 0 : 1;
 
 #if defined(MFX_ENABLE_HEVCE_WEIGHTED_PREDICTION)
     if (platform >= MFX_HW_ICL)
@@ -125,6 +131,36 @@ mfxStatus HardcodeCaps(MFX_ENCODE_CAPS_HEVC& caps, VideoCORE* core)
     if (!caps.ddi_caps.LCUSizeSupported)
         caps.ddi_caps.LCUSizeSupported = 2;
     caps.ddi_caps.BlockSize = 2; // 32x32
+    // align with Windows for Gen12+
+    if (platform >= MFX_HW_TGL_LP)
+    {   // taken from  Windows TGLLP (temporarily)
+        caps.ddi_caps.CodingLimitSet = 1; // = 0 now but should be always set to 1 according to DDI (what about Linux ???)
+        caps.ddi_caps.Color420Only = 0;  // = 1 now
+        caps.ddi_caps.YUV422ReconSupport = 1; // = 0 now
+        caps.ddi_caps.SliceIPBOnly = 1;  // = 0 now (SliceIP is also 0)cz
+        caps.ddi_caps.NoWeightedPred = 0; // = 1 now
+        caps.ddi_caps.NoMinorMVs = 1;  // = 0 now
+        caps.ddi_caps.RawReconRefToggle = 1;  // = 0 now
+        caps.ddi_caps.NoInterlacedField = 1;  // = 0 now
+//        caps.RollingIntraRefresh = 0;  // = 1 now
+//        caps.VCMBitRateControl = 0;  // = 1 now
+        caps.ddi_caps.ParallelBRC = 1;  // = 0 now
+        caps.ddi_caps.LumaWeightedPred = 1; // = 0 now
+        caps.ddi_caps.ChromaWeightedPred = 0; // = 0 now
+        caps.ddi_caps.MaxEncodedBitDepth = 2;  // = 1 now (8/10b only)
+        caps.ddi_caps.MaxPicWidth = 16384;  // = 8192 now
+        caps.ddi_caps.MaxPicHeight = 16384;  // = 8192 now
+        caps.ddi_caps.MaxNumOfROI = 16;  // = 0 now
+        caps.ddi_caps.ROIDeltaQPSupport = 1;  // = 0 now
+        caps.ddi_caps.BlockSize = 1;  // = 0 now (set to 16x16 like in Win)
+        caps.ddi_caps.SliceLevelReportSupport = 1;  // = 0 now
+        caps.ddi_caps.FrameSizeToleranceSupport = 1;  // = 0 now
+        caps.ddi_caps.NumScalablePipesMinus1 = 1;  // = 0 now
+        caps.ddi_caps.MaxNum_WeightedPredL0 = 4; // = 0 now
+        caps.ddi_caps.MaxNum_WeightedPredL1 = 2; // = 0 now
+        caps.ddi_caps.TileSupport = 1;
+        caps.ddi_caps.IntraRefreshBlockUnitSize = 2;
+    }
     (void)core;
 #endif
 

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -521,6 +521,15 @@ mfxU16 MakeSlices(MfxVideoParam& par, mfxU32 SliceStructure)
     if (nTile == 1) //TileScan = RasterScan, no SegmentAddress conversion required
         return (mfxU16)AddTileSlices(par, SliceStructure, nCol, nRow, nSlice);
 
+    if (nTile > 1 && nSlice > nTile && IsOn(par.mfx.LowPower))
+    {
+        // DDI:0.9972: VDEnc supports multiple static slices in case of single tile frame only.
+        //             But for multi tiles frame, no single tile can contain multiple static slices.
+        // Actually it's no matter for driver whether dynamic or static slices are used
+        // It returns an error in the both cases.
+        nSlice = nTile;
+    }
+
     std::vector<mfxU32> colWidth(nTCol, 0);
     std::vector<mfxU32> rowHeight(nTRow, 0);
     std::vector<mfxU32> colBd(nTCol+1, 0);
@@ -956,7 +965,8 @@ mfxStatus CheckAndFixRoi(MfxVideoParam  const & par, ENCODE_CAPS_HEVC const & ca
 
     if (!(par.mfx.RateControlMethod == MFX_RATECONTROL_CBR ||
         par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-        par.mfx.RateControlMethod == MFX_RATECONTROL_CQP))
+        par.mfx.RateControlMethod == MFX_RATECONTROL_CQP ||
+        par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR))
     {
         invalid++;
     }

commit 6b5eb2e5f22794e5afcc51310535df3e69c1cccc

vp9d: fix regression caused by commit 444ecb4
The Shift is wrong since commit 444ecb4 for VP9 10bit streams and
I saw errors below when running a gst-msdk pipeline:

gst-launch-1.0 filesrc location=input.10bit.ivf ! ivfparse ! msdkvp9dec ! fakesink

Redistribute latency...
0:00:00.021322781 15413 0x557733521050 ERROR                msdkdec gstmsdkdec.c:396:gst_msdkdec_init_decoder:<msdkvp9dec0> Init failed (invalid video parameters)
0:00:00.021337017 15413 0x557733521050 ERROR                msdkdec gstmsdkdec.c:868:gst_msdkdec_negotiate:<msdkvp9dec0> Failed to re-negotiate

Fix https://github.com/Intel-Media-SDK/MediaSDK/issues/1790

_studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
@@ -158,6 +158,7 @@ namespace MFX_VPX_Utility
 
             p_out->mfx.FrameInfo.BitDepthLuma   = p_in->mfx.FrameInfo.BitDepthLuma;
             p_out->mfx.FrameInfo.BitDepthChroma = p_in->mfx.FrameInfo.BitDepthChroma;
+            p_out->mfx.FrameInfo.Shift          = p_in->mfx.FrameInfo.Shift;
 
             if ((p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_NV12
                 || p_in->mfx.FrameInfo.FourCC == MFX_FOURCC_AYUV) &&