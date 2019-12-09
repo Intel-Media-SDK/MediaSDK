Intel-Media-SDK/MediaSDK

commit 9fd26ab972e9f7a2bd74e242827a26bc78330872

API 1.31 - JSL
Fixes: #1808

_studio/shared/src/cm_mem_copy.cpp
@@ -2662,12 +2662,15 @@ mfxStatus CmCopyWrapper::InitializeSwapKernels(eMFXHWType hwtype)
         cmSts = m_pCmDevice->LoadProgram((void*)genx_copy_kernel_gen11,sizeof(genx_copy_kernel_gen11),m_pCmProgram,"nojitter");
         break;
     case MFX_HW_ICL_LP:
+#if (MFX_VERSION >= 1031)
+    case MFX_HW_JSL:
     case MFX_HW_EHL:
         cmSts = m_pCmDevice->LoadProgram((void*)genx_copy_kernel_gen11lp,sizeof(genx_copy_kernel_gen11lp),m_pCmProgram,"nojitter");
         break;
     case MFX_HW_TGL_LP:
         cmSts = m_pCmDevice->LoadProgram((void*)genx_copy_kernel_gen12lp,sizeof(genx_copy_kernel_gen12lp),m_pCmProgram,"nojitter");
         break;
+#endif
 #endif
     default:
         cmSts = CM_FAILURE;

_studio/mfx_lib/encode_hw/h265/src/mfx_h265_encode_hw.cpp
@@ -223,7 +223,7 @@ mfxStatus SetLowpowerDefault(MfxVideoParam& par)
     }
 #endif // MFX_VERSION >= 1025
 #if (MFX_VERSION >= 1031)
-    if ((par.m_platform == MFX_HW_EHL)
+    if ((par.m_platform == MFX_HW_JSL || par.m_platform == MFX_HW_EHL)
         && par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
     {
         par.mfx.LowPower = MFX_CODINGOPTION_ON;

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -1416,6 +1416,14 @@ mfxStatus MfxHwH264Encode::SetLowPowerDefault(MfxVideoParam& par, const eMFXHWTy
 {
     mfxStatus sts = CheckTriStateOption(par.mfx.LowPower) == false ? MFX_WRN_INCOMPATIBLE_VIDEO_PARAM : MFX_ERR_NONE;
     (void)platfrom; // fix wrn for non Gen11 build
+#if (MFX_VERSION >= 1031)
+    if ((platfrom == MFX_HW_JSL || platfrom == MFX_HW_EHL)
+        && par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
+    {
+        par.mfx.LowPower = MFX_CODINGOPTION_ON;
+        return sts;
+    }
+#endif
 
     if (par.mfx.LowPower == MFX_CODINGOPTION_UNKNOWN)
         par.mfx.LowPower = MFX_CODINGOPTION_OFF;

_studio/shared/include/mfxstructures-int.h
@@ -69,8 +69,11 @@ enum eMFXHWType
     MFX_HW_ICL       = 0x1400000,
     MFX_HW_ICL_LP    = MFX_HW_ICL + 1,
 
+    MFX_HW_JSL       = 0x1500001,
     MFX_HW_EHL       = 0x1500002,
-    MFX_HW_TGL_LP    = 0x1600000
+    
+    MFX_HW_TGL_LP    = 0x1600000,
+
 };
 
 enum eMFXGTConfig

_studio/shared/src/libmfx_core.cpp
@@ -785,8 +785,12 @@ mfxStatus CommonCORE::QueryPlatform(mfxPlatform* platform)
 #if (MFX_VERSION >= 1027)
     case MFX_HW_ICL    :
     case MFX_HW_ICL_LP : platform->CodeName = MFX_PLATFORM_ICELAKE;       break;
+#endif
+#if (MFX_VERSION >= 1031)
+    case MFX_HW_JSL    : platform->CodeName = MFX_PLATFORM_JASPERLAKE;    break;
+    case MFX_HW_EHL    : platform->CodeName = MFX_PLATFORM_ELKHARTLAKE;   break;
     case MFX_HW_TGL_LP : platform->CodeName = MFX_PLATFORM_TIGERLAKE;     break;
-    case MFX_HW_EHL    : platform->CodeName = MFX_PLATFORM_ELKHARTLAKE; break;
+
 #endif
     default:
 #if (MFX_VERSION >= 1031)

_studio/shared/src/libmfx_core_vaapi.cpp
@@ -327,6 +327,12 @@ typedef struct {
     { 0x8A5D, MFX_HW_ICL_LP, MFX_GT1 },
     { 0x8A71, MFX_HW_ICL_LP, MFX_GT1 },  // GT05, but 1 ok in this context
 
+    /* JSL */
+    { 0x4500, MFX_HW_JSL, MFX_GT2 },
+    { 0x4E51, MFX_HW_JSL, MFX_GT2 },
+    { 0x4E61, MFX_HW_JSL, MFX_GT2 },
+    { 0x4E71, MFX_HW_JSL, MFX_GT2 },
+
     /* EHL */
     { 0x4500, MFX_HW_EHL, MFX_GT2 },
     { 0x4541, MFX_HW_EHL, MFX_GT2 },

_studio/shared/umc/codec/h264_dec/src/umc_h264_mfx_supplier.cpp
@@ -516,7 +516,10 @@ eMFXPlatform MFX_Utility::GetPlatform(VideoCORE * core, mfxVideoParam * par)
     case MFX_HW_CNL:
     case MFX_HW_ICL:
     case MFX_HW_ICL_LP:
+#if (MFX_VERSION >= 1031)	
+    case MFX_HW_JSL:
     case MFX_HW_EHL:
+#endif
         name = sDXVA2_ModeH264_VLD_NoFGT;
         break;
     default:

api/include/mfxcommon.h
@@ -176,6 +176,7 @@ enum {
 #if (MFX_VERSION >= 1027)
     MFX_PLATFORM_ICELAKE        = 30,
 #endif
+    MFX_PLATFORM_JASPERLAKE     = 32,
     MFX_PLATFORM_ELKHARTLAKE    = 33,
     MFX_PLATFORM_TIGERLAKE      = 40,
 };

commit 0bfd782185b045563fd635e9127cebaa2da6f977

1.27: fix cm mem copy for icl
Fixes: 9fd26ab ("API 1.31 - JSL")

Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

_studio/shared/src/cm_mem_copy.cpp
@@ -2665,8 +2665,10 @@ mfxStatus CmCopyWrapper::InitializeSwapKernels(eMFXHWType hwtype)
 #if (MFX_VERSION >= 1031)
     case MFX_HW_JSL:
     case MFX_HW_EHL:
+#endif
         cmSts = m_pCmDevice->LoadProgram((void*)genx_copy_kernel_gen11lp,sizeof(genx_copy_kernel_gen11lp),m_pCmProgram,"nojitter");
         break;
+#if (MFX_VERSION >= 1031)
     case MFX_HW_TGL_LP:
         cmSts = m_pCmDevice->LoadProgram((void*)genx_copy_kernel_gen12lp,sizeof(genx_copy_kernel_gen12lp),m_pCmProgram,"nojitter");
         break;