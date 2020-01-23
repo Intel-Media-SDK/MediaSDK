Intel-Media-SDK/MediaSDK

commit 7eb0c0b88fd57e3148b16496f8ad9b90f5a21485

core: Use default allocator for allocate internal resources
We use default allocator, if external allocator fails (also may leads
fake errs). So better to use default allocator right away for internal
recources.

fixes: #1800

_studio/shared/src/libmfx_core_vaapi.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2019 Intel Corporation
+// Copyright (c) 2017-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -612,54 +612,35 @@ VAAPIVideoCORE::AllocFrames(
         } else
         {
             // external allocator
-            if (m_bSetExtFrameAlloc && request->Info.FourCC != MFX_FOURCC_P8)
+            if (m_bSetExtFrameAlloc &&
+                request->Info.FourCC != MFX_FOURCC_P8 &&
+                (request->Type & MFX_MEMTYPE_EXTERNAL_FRAME))
             {
+                // make 'fake' Alloc call to retrieve memId's of surfaces already allocated by app.
                 sts = (*m_FrameAllocator.frameAllocator.Alloc)(m_FrameAllocator.frameAllocator.pthis, &temp_request, response);
 
-                // if external allocator cannot allocate d3d frames - use default memory allocator
-                if (MFX_ERR_UNSUPPORTED == sts || MFX_ERR_MEMORY_ALLOC == sts)
-                {
-                    // Default Allocator is used for internal memory allocation only
-                    if (request->Type & MFX_MEMTYPE_EXTERNAL_FRAME)
-                        return sts;
-                    m_bUseExtAllocForHWFrames = false;
-                    sts = DefaultAllocFrames(request, response);
-                    MFX_CHECK_STS(sts);
+                m_bUseExtAllocForHWFrames = false;
+                MFX_CHECK_STS(sts);
 
-                    return TraceFrames(request, response, sts);
-                }
                 // let's create video accelerator
-                else if (MFX_ERR_NONE == sts)
+                // Checking for unsupported mode - external allocator exist but Device handle doesn't set
+                MFX_CHECK(m_Display, MFX_ERR_UNSUPPORTED)
+
+                if (response->NumFrameActual < request->NumFrameMin)
                 {
-                    // Checking for unsupported mode - external allocator exist but Device handle doesn't set
-                    if (!m_Display)
-                        return MFX_ERR_UNSUPPORTED;
-
-                    if (response->NumFrameActual < request->NumFrameMin)
-                    {
-                        (*m_FrameAllocator.frameAllocator.Free)(m_FrameAllocator.frameAllocator.pthis, response);
-                        return MFX_ERR_MEMORY_ALLOC;
-                    }
-
-                    m_bUseExtAllocForHWFrames = true;
-                    sts = ProcessRenderTargets(request, response, &m_FrameAllocator);
-                    MFX_CHECK_STS(sts);
-
-                    return TraceFrames(request, response, sts);
+                    (*m_FrameAllocator.frameAllocator.Free)(m_FrameAllocator.frameAllocator.pthis, response);
+                    MFX_RETURN(MFX_ERR_MEMORY_ALLOC);
                 }
-                // error situation
-                else
-                {
-                    m_bUseExtAllocForHWFrames = false;
-                    return sts;
-                }
+
+                m_bUseExtAllocForHWFrames = true;
+                sts = ProcessRenderTargets(request, response, &m_FrameAllocator);
+                MFX_CHECK_STS(sts);
+
+                return TraceFrames(request, response, sts);
             }
             else
             {
                 // Default Allocator is used for internal memory allocation and all coded buffers allocation
-                if (request->Type & MFX_MEMTYPE_EXTERNAL_FRAME)
-                    return MFX_ERR_MEMORY_ALLOC;
-
                 m_bUseExtAllocForHWFrames = false;
                 sts = DefaultAllocFrames(request, response);
                 MFX_CHECK_STS(sts);

commit a8c2ae2524f52d4c102f9ec145755f413b655622

core: Use external allocator for find FEI ENC / PAK reconstructs
FEI ENC/PAK uses external allocator to retrieve memId's of surfaces
which already allocated by app

fixes: 7eb0c0b (core: Use default allocator for allocate internal
resources")

_studio/shared/src/libmfx_core_vaapi.cpp
@@ -614,7 +614,7 @@ VAAPIVideoCORE::AllocFrames(
             // external allocator
             if (m_bSetExtFrameAlloc &&
                 request->Info.FourCC != MFX_FOURCC_P8 &&
-                (request->Type & MFX_MEMTYPE_EXTERNAL_FRAME))
+                (request->Type & (MFX_MEMTYPE_EXTERNAL_FRAME | MFX_MEMTYPE_FROM_ENC | MFX_MEMTYPE_FROM_PAK)))
             {
                 // make 'fake' Alloc call to retrieve memId's of surfaces already allocated by app.
                 sts = (*m_FrameAllocator.frameAllocator.Alloc)(m_FrameAllocator.frameAllocator.pthis, &temp_request, response);