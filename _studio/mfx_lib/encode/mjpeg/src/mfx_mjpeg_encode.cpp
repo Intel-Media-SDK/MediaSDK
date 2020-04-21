Intel-Media-SDK/MediaSDK

commit 186bbd6d0516ea42fc66dfa7a61f08cb509eac3a

[jpegd][jpege] Check that SW fallback is supported before using it
jpeg software fallback is supported starting SSE4.2.

_studio/mfx_lib/encode/mjpeg/src/mfx_mjpeg_encode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 Intel Corporation
+// Copyright (c) 2018-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,8 @@
 #if defined (MFX_ENABLE_MJPEG_VIDEO_ENCODE)
 
 #include <thread> // for thread::hardware_concurrency()
+
+#include <ippcore.h> // for mfxInit()
 
 #include "mfx_common.h"
 
@@ -1102,13 +1104,14 @@ mfxStatus MFXVideoENCODEMJPEG::Init(mfxVideoParam *par_in)
     mfxStatus st = MFX_ERR_NONE, QueryStatus = MFX_ERR_NONE;
     mfxVideoParam* par = par_in;
 
-    if(m_isInitialized)
-        return MFX_ERR_UNDEFINED_BEHAVIOR;
+    MFX_CHECK(m_isInitialized, MFX_ERR_UNDEFINED_BEHAVIOR);
+    MFX_CHECK_NULL_PTR1(par);
 
-    if(par == NULL)
-        return MFX_ERR_NULL_PTR;
+    auto ippSt = mfxInit();
+    MFX_CHECK(ippSt == ippStsNoErr, MFX_ERR_UNSUPPORTED);
 
-    MFX_CHECK(CheckExtBuffers(par->ExtParam, par->NumExtParam)== MFX_ERR_NONE, MFX_ERR_INVALID_VIDEO_PARAM);
+    st = CheckExtBuffers(par->ExtParam, par->NumExtParam);
+    MFX_CHECK(st == MFX_ERR_NONE, MFX_ERR_INVALID_VIDEO_PARAM);
 
     mfxExtJPEGQuantTables*    jpegQT       = (mfxExtJPEGQuantTables*)   GetExtBuffer( par->ExtParam, par->NumExtParam, MFX_EXTBUFF_JPEG_QT );
     mfxExtJPEGHuffmanTables*  jpegHT       = (mfxExtJPEGHuffmanTables*) GetExtBuffer( par->ExtParam, par->NumExtParam, MFX_EXTBUFF_JPEG_HUFFMAN );
@@ -1348,13 +1351,11 @@ mfxStatus MFXVideoENCODEMJPEG::Reset(mfxVideoParam *par)
 {
     mfxStatus st, QueryStatus;
 
-    if(!m_isInitialized)
-        return MFX_ERR_NOT_INITIALIZED;
+    MFX_CHECK(m_isInitialized, MFX_ERR_NOT_INITIALIZED);
+    MFX_CHECK_NULL_PTR1(par);
 
-    if(par == NULL)
-        return MFX_ERR_NULL_PTR;
-
-    MFX_CHECK(CheckExtBuffers(par->ExtParam, par->NumExtParam)== MFX_ERR_NONE, MFX_ERR_INVALID_VIDEO_PARAM);
+    st = CheckExtBuffers(par->ExtParam, par->NumExtParam);
+    MFX_CHECK(st == MFX_ERR_NONE, MFX_ERR_INVALID_VIDEO_PARAM);
 
     mfxExtJPEGQuantTables*    jpegQT       = (mfxExtJPEGQuantTables*)   GetExtBuffer( par->ExtParam, par->NumExtParam, MFX_EXTBUFF_JPEG_QT );
     mfxExtJPEGHuffmanTables*  jpegHT       = (mfxExtJPEGHuffmanTables*) GetExtBuffer( par->ExtParam, par->NumExtParam, MFX_EXTBUFF_JPEG_HUFFMAN );

_studio/mfx_lib/decode/mjpeg/src/mfx_mjpeg_dec_decode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 Intel Corporation
+// Copyright (c) 2018-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -28,6 +28,7 @@
 #include "mfx_common_decode_int.h"
 
 #ifdef MFX_ENABLE_SW_FALLBACK
+#include <ippcore.h> // for mfxInit()
 #include "mfx_mjpeg_task.h"
 #include "umc_mjpeg_mfx_decode.h"
 #include "mfx_thread_task.h"
@@ -2019,6 +2020,9 @@ VideoDECODEMJPEGBase_SW::VideoDECODEMJPEGBase_SW()
 
 mfxStatus VideoDECODEMJPEGBase_SW::Init(mfxVideoParam *decPar, mfxFrameAllocRequest *request, mfxFrameAllocResponse *response, mfxFrameAllocRequest *, bool isUseExternalFrames, VideoCORE *core)
 {
+    auto ippSt = mfxInit();
+    MFX_CHECK(ippSt == ippStsNoErr, MFX_ERR_UNSUPPORTED);
+
     UMC::Status umcSts = m_FrameAllocator->InitMfx(0, core, decPar, request, response, isUseExternalFrames, true);
     MFX_CHECK(umcSts == UMC::UMC_OK, MFX_ERR_MEMORY_ALLOC);
 

contrib/ipp/include/ippcore.h
@@ -80,6 +80,8 @@ IPPAPI( void*, mfxMalloc,  (int length) )
 IPPAPI( void, mfxFree, (void* ptr) )
 
 
+IPPAPI( IppStatus, mfxInit, (void) )
+
 /* ////////////////////////////////////////////////////////////////////////////
 //  Name:       mfxGetMaxCacheSizeB
 //

contrib/ipp/src/ippinit.c
@@ -1,0 +1,29 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "ippcore.h"
+#include "owndefs.h"
+
+IPPFUN( IppStatus, mfxInit, (void) )
+{
+    return __builtin_cpu_supports("sse4.2") > 0? ippStsNoErr : ippStsNotSupportedCpu;
+}
+
+

commit bf59394a8910f8fdf54b48024f01e964d285d0f1

[jpege] Fix SW fallback initialization
It was broken by 186bbd6d.

_studio/mfx_lib/encode/mjpeg/src/mfx_mjpeg_encode.cpp
@@ -1104,7 +1104,7 @@ mfxStatus MFXVideoENCODEMJPEG::Init(mfxVideoParam *par_in)
     mfxStatus st = MFX_ERR_NONE, QueryStatus = MFX_ERR_NONE;
     mfxVideoParam* par = par_in;
 
-    MFX_CHECK(m_isInitialized, MFX_ERR_UNDEFINED_BEHAVIOR);
+    MFX_CHECK(!m_isInitialized, MFX_ERR_UNDEFINED_BEHAVIOR);
     MFX_CHECK_NULL_PTR1(par);
 
     auto ippSt = mfxInit();