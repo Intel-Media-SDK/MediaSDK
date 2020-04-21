080c6e42ca56100789d687124e8dd9b0446a978e
Yang, Ning <ning.yang@intel.com>
2020-04-21 19:44:34 +0800

    [HEVCd] Fix display order corruption
    Modify random access code logic

--- _studio/shared/umc/codec/h265_dec/include/umc_h265_task_supplier.h
+++ _studio/shared/umc/codec/h265_dec/include/umc_h265_task_supplier.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Intel Corporation
+// Copyright (c) 2020 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -438,6 +438,7 @@ protected:
     bool        m_use_external_framerate;
     bool        m_decodedOrder;
     bool        m_checkCRAInsideResetProcess;
+    bool        m_bFirstSliceInSequence;
 
     H265Slice * m_pLastSlice;
 
