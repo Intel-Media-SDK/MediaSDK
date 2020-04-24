f9b90664c4443062ae94b0f19d773dd197054b26
Sebastian Possos <sebastian.possos@intel.com>
2020-04-24 14:21:47 -0700

    [VPP][ASC] SSE3 and SSE4 instruction set dispatch
    Moved SSE3 and SSE4 optimized code to SSE4
    implementation specific file, code cleanup
    of unused functions and check of SSE4 availability
    on runtime platform
    
    Test: vpp_sc_di
    
    Signed-off-by: Sebastian Possos <sebastian.possos@intel.com>

--- _studio/shared/asc/include/motion_estimation_engine.h
+++ _studio/shared/asc/include/motion_estimation_engine.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2020 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -27,15 +27,18 @@ namespace ns_asc {
 
 void MotionRangeDeliveryF(mfxI16 xLoc, mfxI16 yLoc, mfxI16 *limitXleft, mfxI16 *limitXright, mfxI16 *limitYup, mfxI16 *limitYdown, ASCImDetails dataIn);
 
-/* 4x4 Block size Functions */
-bool MVcalcSAD4x4(ASCMVector MV, pmfxU8 curY, pmfxU8 refY, ASCImDetails dataIn, mfxU32 *bestSAD, mfxI32 *distance);
-/* 8x8 Block size Functions */
-bool MVcalcSAD8x8(ASCMVector MV, pmfxU8 curY, pmfxU8 refY, ASCImDetails *dataIn, mfxU32 *bestSAD, mfxI32 *distance);
-mfxU16 __cdecl ME_simple(ASCVidRead *videoIn, mfxI32 fPos, ASCImDetails *dataIn, ASCimageData *scale, ASCimageData *scaleRef, bool first, ASCVidData *limits, t_ME_SAD_8x8_Block_Search ME_SAD_8x8_Block_Search);
-/* All Block sizes -general case- */
-bool MVcalcSAD(ASCMVector MV, pmfxU8 curY, pmfxU8 refY, ASCImDetails dataIn, mfxI32 fPos,mfxI32 xLoc, mfxI32 yLoc, mfxU32 *bestSAD, mfxI32 *distance);
-/* ------------------------------ */
-void SearchLimitsCalc(mfxI32 xLoc, mfxI32 yLoc, mfxI32 *limitXleft, mfxI32 *limitXright, mfxI32 *limitYup, mfxI32 *limitYdown, ASCImDetails dataIn, mfxI32 range, ASCMVector mv, ASCVidData limits);
+mfxU16 __cdecl ME_simple(
+    ASCVidRead *videoIn,
+    mfxI32 fPos,
+    ASCImDetails *dataIn,
+    ASCimageData *scale,
+    ASCimageData *scaleRef,
+    bool first,
+    ASCVidData *limits,
+    t_ME_SAD_8x8_Block_Search ME_SAD_8x8_Block_Search,
+    t_ME_SAD_8x8_Block ME_SAD_8x8_opt,
+    t_ME_VAR_8x8_Block ME_VAR_8x8_opt
+);
 mfxF32 Dist(ASCMVector vector);
 mfxI32 DistInt(ASCMVector vector);
 void MVpropagationCheck(mfxI32 xLoc, mfxI32 yLoc, ASCImDetails dataIn, ASCMVector *propagatedMV);
