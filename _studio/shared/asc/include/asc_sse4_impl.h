74bc125b7a7de2a1d9addbdc98f9bb276ab38136
Sebastian Possos <sebastian.possos@intel.com>
2020-04-24 14:21:47 -0700

    [VPP][ASC] SSE3 and SSE4 instruction set dispatch
    Moved SSE3 and SSE4 optimized code to SSE4
    implementation specific file, code cleanup
    of unused functions and check of SSE4 availability
    on runtime platform
    
    Test: vpp_sc_di
    
    Signed-off-by: Sebastian Possos <sebastian.possos@intel.com>

--- _studio/shared/asc/include/asc_sse4_impl.h
+++ _studio/shared/asc/include/asc_sse4_impl.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2020 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,15 @@
 #ifndef _ASC_SSE4_IMPL_H_
 #define _ASC_SSE4_IMPL_H_
 #include "asc_common_impl.h"
+
+#define _mm_loadh_epi64(a, ptr) _mm_castps_si128(_mm_loadh_pi(_mm_castsi128_ps(a), (__m64 *)(ptr)))
+#define _mm_movehl_epi64(a, b) _mm_castps_si128(_mm_movehl_ps(_mm_castsi128_ps(a), _mm_castsi128_ps(b)))
+
+/*ME_SAD_8x8_Block_SSE4 and ME_VAR_8x8_Block_SSE4 also use SSE3 instructions but because
+they include SSE4 instructions too then they have been integrated here*/
+mfxU16 ME_SAD_8x8_Block_SSE4(mfxU8 *pSrc, mfxU8 *pRef, mfxU32 srcPitch, mfxU32 refPitch);
+void ME_VAR_8x8_Block_SSE4(mfxU8 *pSrc, mfxU8 *pRef, mfxU8 *pMCref, mfxI16 srcAvgVal,
+    mfxI16 refAvgVal, mfxU32 srcPitch, mfxU32 refPitch, mfxI32 &var, mfxI32 &jtvar, mfxI32 &jtMCvar);
 
 void ME_SAD_8x8_Block_Search_SSE4(mfxU8 *pSrc, mfxU8 *pRef, int pitch, int xrange, int yrange,
     mfxU16 *bestSAD, int *bestX, int *bestY);
