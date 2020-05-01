215e2e17955ad66158dc58736e3a4ff9a2bcd3d8
Sebastian Possos <sebastian.possos@intel.com>
2020-04-30 18:27:10 -0700

    [VPP][ASC] remove/hide asc instruction-set specific code
    Moved optimized functions located in asc_common_impl.h
    to their respective optimization file. These functions
    deal with incomplete lines for full AVX2 and SSE4.1
    registers.
    
    Related to: #2104
    Test: vpp_sc_di
    
    Signed-off-by: Sebastian Possos <sebastian.possos@intel.com>

--- _studio/shared/asc/src/asc_common_impl.cpp
+++ _studio/shared/asc/src/asc_common_impl.cpp
@@ -1,15 +1,16 @@
+/*//////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2018-2020 Intel Corporation
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,33 +18,16 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+*/
+#include "asc_common_impl.h"
 
-#ifndef _ASC_COMMON_IMPL_H_
-#define _ASC_COMMON_IMPL_H_
-
-#include "asc_defs.h"
-#include "asc_structures.h"
-
-static const int HIST_THRESH_LO = 1;
-static const int HIST_THRESH_HI = 12;
-#define SAD_SEARCH_VSTEP 2  // 1=FS 2=FHS
-
-ASC_ALIGN_DECL(16) static const mfxU16 tab_twostep[8] = {
-    0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff,
-};
-
-ASC_ALIGN_DECL(16) static const mfxU16 tab_killmask[8][8] = {
-    { 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff },
-    { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff },
-};
-
-void calc_RACA_4x4_C(mfxU8 *pSrc, mfxI32 pitch, mfxI32 *RS, mfxI32 *CS) {
+void calc_RACA_4x4_C(
+    mfxU8  * pSrc,
+    mfxI32   pitch,
+    mfxI32 * RS,
+    mfxI32 * CS
+)
+{
     mfxI32 i, j;
     mfxU8 *pS = pSrc;
     mfxU8 *pS2 = pSrc + pitch;
@@ -65,5 +49,3 @@ void calc_RACA_4x4_C(mfxU8 *pSrc, mfxI32 pitch, mfxI32 *RS, mfxI32 *CS) {
     *CS += Cs >> 4;
     *RS += Rs >> 4;
 }
-
-#endif //_ASC_COMMON_IMPL_H_
