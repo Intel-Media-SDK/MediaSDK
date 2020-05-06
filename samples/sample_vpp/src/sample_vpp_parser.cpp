2d7be641aa05094364190f662702c7c9e2073392
Ruan, Hongfu <hongfu.ruan@intel.com>
2020-05-06 17:20:49 +0800

    fix compile error
    std::ptr_fun was deprecated in C++11 and removed in C++17, use lambda instead.
    
    Signed-off-by: Ruan, Hongfu <hongfu.ruan@intel.com>

--- samples/sample_vpp/src/sample_vpp_parser.cpp
+++ samples/sample_vpp/src/sample_vpp_parser.cpp
@@ -2002,13 +2002,13 @@ bool CheckInputParams(msdk_char* strInput[], sInputParams* pParams )
 
 // trim from start
 static inline std::string &ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c){ return !std::isspace(c); }));
     return s;
 }
 
 // trim from end
 static inline std::string &rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char c){ return !std::isspace(c); }).base(), s.end());
     return s;
 }
 
