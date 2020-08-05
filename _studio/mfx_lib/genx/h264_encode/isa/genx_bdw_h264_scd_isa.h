16743e7d9e7fb2cdc3e512cc1920c84fae06914a
Alexander Smirnov <alexander.smirnov@intel.com>
2020-08-05 22:20:09 +0300

    [CM][AVC LA] Fix PictureWidth value
    This change makes it possible to use 4k streams with AVC LA
    Now work with PictureWidth value is implemented same as PictureHeightMinusOne
    Previously, overflow happens when dividing 4096 / 16, now it is fixed
    CM kernels for all platforms were re-compiled.

--- _studio/mfx_lib/genx/h264_encode/isa/genx_bdw_h264_scd_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_bdw_h264_scd_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Intel Corporation
+// Copyright (c) 2018-2020 Intel Corporation
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +19,5 @@
 // SOFTWARE.
 #ifndef __genx_bdw_h264_scd__
 #define __genx_bdw_h264_scd__
-extern const unsigned char genx_bdw_h264_scd[18014];
+extern const unsigned char genx_bdw_h264_scd[19462];
 #endif
--- _studio/mfx_lib/genx/h264_encode/isa/genx_histogram_gen11_isa.h
+++ _studio/mfx_lib/genx/h264_encode/isa/genx_histogram_gen11_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2018 Intel Corporation
+// Copyright (c) 2012-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,5 +20,5 @@
 
 #ifndef __genx_histogram_gen11__
 #define __genx_histogram_gen11__
-extern const unsigned char genx_histogram_gen11[62902];
+extern const unsigned char genx_histogram_gen11[59907];
 #endif
