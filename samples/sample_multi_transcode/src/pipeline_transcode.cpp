401dba1b687b7665616b5dc5c5f80d84ad56ca5c
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2020-08-11 18:24:37 +0300

    [smt] Fix uninitialized variable in constructor

--- samples/sample_multi_transcode/src/pipeline_transcode.cpp
+++ samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -146,6 +146,7 @@ CTranscodingPipeline::CTranscodingPipeline():
     m_NumFramesForReset(0),
     isHEVCSW(false),
     m_bInsertIDR(false),
+    m_rawInput(false),
     m_FrameNumberPreference(0xFFFFFFFF),
     m_MaxFramesForTranscode(0xFFFFFFFF),
     m_pBSProcessor(NULL),
