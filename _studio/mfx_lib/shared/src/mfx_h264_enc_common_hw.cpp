Intel-Media-SDK/MediaSDK

commit 9188b153c8ba5129ab8b49b9de04871c7b3e6747

[h264e] Return UNSUPPORTED, if sliding window is not supported by caps
Issue: MDP-62514

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -3277,6 +3277,12 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         extOpt2->MBBRC = MFX_CODINGOPTION_OFF;
     }
 
+    if (extOpt3->WinBRCSize && !hwCaps.ddi_caps.FrameSizeToleranceSupport)
+    {
+        unsupported = true;
+        extOpt3->WinBRCSize = 0;
+    }
+
     if (extOpt2->BRefType > 2)
     {
         changed = true;

_studio/mfx_lib/shared/src/mfx_h264_encode_vaapi.cpp
@@ -231,7 +231,7 @@ mfxStatus SetRateControl(
 
     rate_param->rc_flags.bits.frame_tolerance_mode = ConvertLowDelayBRCMfx2Ddi(extOpt3.LowDelayBRC, par.calcParam.TCBRCTargetFrameSize);
     // Activate frame tolerance sliding window mode
-    if (extOpt3.WinBRCSize && caps.ddi_caps.FrameSizeToleranceSupport)
+    if (extOpt3.WinBRCSize)
     {
         rate_param->rc_flags.bits.frame_tolerance_mode = eFrameSizeTolerance_Low;
     }

commit ab651f0ef5f7af27f57fb3e699819c7dfa3f5b86

[h264e] Fix sliding window for MFX_RATECONTROL_LA*
1) Sliding window for MFX_RATECONTROL_LA* doesn't require driver
support. It was broken in 9188b153c.
2) Sliding window for VBR and QVBR requires driver support along with
CBR.
3) WinBRCSize==0 better matches unsupported, not changed, as the
functionality is not supported.

Issue: MDP-62514, VSMGWL-29997

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -3283,12 +3283,6 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         extOpt2->MBBRC = MFX_CODINGOPTION_OFF;
     }
 
-    if (extOpt3->WinBRCSize && !hwCaps.ddi_caps.FrameSizeToleranceSupport)
-    {
-        unsupported = true;
-        extOpt3->WinBRCSize = 0;
-    }
-
     if (extOpt2->BRefType > 2)
     {
         changed = true;
@@ -4539,17 +4533,17 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         changed = true;
     }
 
-    bool mfxRateControlHwCbr =
-        (platform == MFX_HW_APL || platform == MFX_HW_CFL) &&
-        (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR);
+    bool mfxRateControlHwSupport =
+        hwCaps.ddi_caps.FrameSizeToleranceSupport &&
+        (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR ||
+         par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
+         par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR);
 
     bool slidingWindowSupported  =
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA  ||
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA_HRD ||
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA_EXT ||
-            par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-            par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR ||
-            (mfxRateControlHwCbr && !IsOn(extOpt2->ExtBRC));
+            (mfxRateControlHwSupport && !IsOn(extOpt2->ExtBRC));
 
      if (extOpt3->WinBRCMaxAvgKbps || extOpt3->WinBRCSize)
      {
@@ -4558,16 +4552,13 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
              extOpt3->WinBRCMaxAvgKbps = 0;
              extOpt3->WinBRCSize = 0;
              par.calcParam.WinBRCMaxAvgKbps = 0;
-             changed = true;
+             unsupported = true;
          }
          else if (extOpt3->WinBRCSize==0)
          {
              warning = true;
          }
-         else if ((par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-             par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR ||
-             mfxRateControlHwCbr) &&
-             !IsOn(extOpt2->ExtBRC))
+         else if (mfxRateControlHwSupport && !IsOn(extOpt2->ExtBRC))
          {
              if (par.mfx.FrameInfo.FrameRateExtN != 0 && par.mfx.FrameInfo.FrameRateExtD != 0)
              {
@@ -4593,7 +4584,7 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
              }
              else if (par.calcParam.WinBRCMaxAvgKbps)
              {
-                 if (mfxRateControlHwCbr &&
+                 if (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR &&
                      par.calcParam.targetKbps &&
                      par.calcParam.WinBRCMaxAvgKbps != par.calcParam.targetKbps)
                  {