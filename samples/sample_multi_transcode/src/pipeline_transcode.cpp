Intel-Media-SDK/MediaSDK

commit 90a079d24e836ce3809ff4d0ee2d7db8e8780803

[sample_multi_transcode] Add support for encoding I420 and NV12 streams
Issue: MDP-61899

samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -545,7 +545,13 @@ mfxStatus CTranscodingPipeline::DecodeOneFrame(ExtendedSurface *pExtSurface)
     DevBusyTimer.Start();
     while (MFX_ERR_MORE_DATA == sts || MFX_ERR_MORE_SURFACE == sts || MFX_ERR_NONE < sts)
     {
-        if (MFX_WRN_DEVICE_BUSY == sts)
+        if (m_rawInput)
+        {
+            pExtSurface->pSurface = GetFreeSurface(false, MSDK_SURFACE_WAIT_INTERVAL);
+            sts = m_pBSProcessor->GetInputFrame(pExtSurface->pSurface);
+            MFX_CHECK_STS(sts);
+        }
+        else if (MFX_WRN_DEVICE_BUSY == sts)
         {
             WaitForDeviceToBecomeFree(*m_pmfxSession,m_LastDecSyncPoint,sts);
         }
@@ -571,7 +577,10 @@ mfxStatus CTranscodingPipeline::DecodeOneFrame(ExtendedSurface *pExtSurface)
             MSDK_CHECK_POINTER_SAFE(pmfxSurface, MFX_ERR_MEMORY_ALLOC, msdk_printf(MSDK_STRING("ERROR: No free surfaces in decoder pool (during long period)\n"))); // return an error if a free surface wasn't found
         }
 
-        sts = m_pmfxDEC->DecodeFrameAsync(m_pmfxBS, pmfxSurface, &pExtSurface->pSurface, &pExtSurface->Syncp);
+        if (!m_rawInput)
+        {
+            sts = m_pmfxDEC->DecodeFrameAsync(m_pmfxBS, pmfxSurface, &pExtSurface->pSurface, &pExtSurface->Syncp);
+        }
 
         if ( (MFX_WRN_DEVICE_BUSY == sts) &&
              (DevBusyTimer.GetTime() > MSDK_WAIT_INTERVAL/1000) )
@@ -620,15 +629,23 @@ mfxStatus CTranscodingPipeline::DecodeLastFrame(ExtendedSurface *pExtSurface)
     // retrieve the buffered decoded frames
     while (MFX_ERR_MORE_SURFACE == sts || MFX_WRN_DEVICE_BUSY == sts)
     {
-        if (MFX_WRN_DEVICE_BUSY == sts)
+        if (m_rawInput)
+        {
+            pExtSurface->pSurface = GetFreeSurface(false, MSDK_SURFACE_WAIT_INTERVAL);
+            sts = m_pBSProcessor->GetInputFrame(pExtSurface->pSurface);
+        }
+        else if (MFX_WRN_DEVICE_BUSY == sts)
         {
             WaitForDeviceToBecomeFree(*m_pmfxSession,m_LastDecSyncPoint,sts);
         }
 
-        // find new working surface
-        pmfxSurface = GetFreeSurface(true, MSDK_SURFACE_WAIT_INTERVAL);
-        MSDK_CHECK_POINTER_SAFE(pmfxSurface, MFX_ERR_MEMORY_ALLOC, msdk_printf(MSDK_STRING("ERROR: No free surfaces in decoder pool (during long period)\n"))); // return an error if a free surface wasn't found
-        sts = m_pmfxDEC->DecodeFrameAsync(NULL, pmfxSurface, &pExtSurface->pSurface, &pExtSurface->Syncp);
+        if (!m_rawInput)
+        {
+            // find new working surface
+            pmfxSurface = GetFreeSurface(true, MSDK_SURFACE_WAIT_INTERVAL);
+            MSDK_CHECK_POINTER_SAFE(pmfxSurface, MFX_ERR_MEMORY_ALLOC, msdk_printf(MSDK_STRING("ERROR: No free surfaces in decoder pool (during long period)\n"))); // return an error if a free surface wasn't found
+            sts = m_pmfxDEC->DecodeFrameAsync(NULL, pmfxSurface, &pExtSurface->pSurface, &pExtSurface->Syncp);
+        }
 
         if ( (MFX_WRN_DEVICE_BUSY == sts) &&
              (DevBusyTimer.GetTime() > MSDK_WAIT_INTERVAL/1000) )
@@ -940,7 +957,7 @@ mfxStatus CTranscodingPipeline::Decode()
             MSDK_BREAK_ON_ERROR(sts);
         }
 
-        if (m_pmfxVPP.get())
+        if (m_pmfxVPP.get() && !m_rawInput)
         {
             if (m_bIsFieldWeaving)
             {
@@ -1725,7 +1742,7 @@ mfxStatus CTranscodingPipeline::Transcode()
             m_mfxDecParams.mfx.FrameInfo.PicStruct = DecExtSurface.pSurface->Info.PicStruct;
         }
         // pre-process a frame
-        if (m_pmfxVPP.get() && bNeedDecodedFrames)
+        if (m_pmfxVPP.get() && bNeedDecodedFrames && !m_rawInput)
         {
             if (m_bIsFieldWeaving)
             {
@@ -2297,6 +2314,11 @@ void CTranscodingPipeline::FillFrameInfoForEncoding(mfxFrameInfo& info, sInputPa
     {
         ConvertFrameRate(pInParams->dVPPOutFramerate, &info.FrameRateExtN, &info.FrameRateExtD);
     }
+    else
+    {
+        info.FrameRateExtN = 30;
+        info.FrameRateExtD = 1;
+    }
 }
 
 mfxStatus CTranscodingPipeline::InitEncMfxParams(sInputParams *pInParams)
@@ -2803,6 +2825,14 @@ mfxStatus CTranscodingPipeline::AddLaStreams(mfxU16 width, mfxU16 height)
     return MFX_ERR_NONE;
 }
 
+mfxU32 CTranscodingPipeline::FileFourCC2EncFourCC(mfxU32 fcc)
+{
+    if (fcc == MFX_FOURCC_I420)
+        return MFX_FOURCC_NV12;
+    else
+        return fcc;
+}
+
 mfxStatus CTranscodingPipeline::InitVppMfxParams(sInputParams *pInParams)
 {
     MSDK_CHECK_POINTER(pInParams,  MFX_ERR_NULL_PTR);
@@ -2831,6 +2861,35 @@ mfxStatus CTranscodingPipeline::InitVppMfxParams(sInputParams *pInParams)
 
     // input frame info
     MSDK_MEMCPY_VAR(m_mfxVppParams.vpp.In, &m_mfxDecParams.mfx.FrameInfo, sizeof(mfxFrameInfo));
+
+    if (m_mfxVppParams.vpp.In.Width * m_mfxVppParams.vpp.In.Height == 0)
+    {
+        m_mfxVppParams.vpp.In.Width = MSDK_ALIGN32(pInParams->nDstWidth);
+        m_mfxVppParams.vpp.In.Height = MSDK_ALIGN32(pInParams->nDstHeight);
+    }
+
+    if (m_mfxVppParams.vpp.In.CropW * m_mfxVppParams.vpp.In.CropH == 0)
+    {
+        m_mfxVppParams.vpp.In.CropW = pInParams->nDstWidth;
+        m_mfxVppParams.vpp.In.CropH = pInParams->nDstHeight;
+    }
+
+    if (m_mfxVppParams.vpp.In.FrameRateExtN * m_mfxVppParams.vpp.In.FrameRateExtD == 0)
+    {
+        m_mfxVppParams.vpp.In.FrameRateExtN = 30;
+        m_mfxVppParams.vpp.In.FrameRateExtD = 1;
+    }
+
+    if (m_mfxVppParams.vpp.In.FourCC == 0)
+    {
+        m_mfxVppParams.vpp.In.FourCC = FileFourCC2EncFourCC(pInParams->DecodeId);
+        m_mfxVppParams.mfx.FrameInfo.ChromaFormat = FourCCToChroma(pInParams->DecoderFourCC);
+    }
+
+    if (m_rawInput)
+    {
+        m_mfxVppParams.mfx.FrameInfo.PicStruct = MFX_PICSTRUCT_PROGRESSIVE;
+    }
 
     // fill output frame info
     MSDK_MEMCPY_VAR(m_mfxVppParams.vpp.Out, &m_mfxVppParams.vpp.In, sizeof(mfxFrameInfo));
@@ -3130,8 +3189,13 @@ mfxStatus CTranscodingPipeline::AllocFrames(mfxFrameAllocRequest *pRequest, bool
         auto surface = new mfxFrameSurfaceWrap;
         surface->Info = pRequest->Info;
 
+        if (m_rawInput)
+        {
+            sts = m_pMFXAllocator->Lock(m_pMFXAllocator->pthis, pResponse->mids[i], &(surface->Data));
+            MSDK_CHECK_STATUS(sts, "m_pMFXAllocator->Lock failed");
+        }
         // no actual memory is allocated if opaque memory type is used (surface pointers and MemId field remain 0)
-        if (!m_bUseOpaqueMemory)
+        else if (!m_bUseOpaqueMemory)
         {
             surface->Data.MemId = pResponse->mids[i];
         }
@@ -3226,7 +3290,7 @@ mfxStatus CTranscodingPipeline::AllocFrames()
            sts = CorrectAsyncDepth(VPPOut, m_AsyncDepth);
            MSDK_CHECK_STATUS(sts, "CorrectAsyncDepth failed");
            /* WA for rendering: VPP should have at least 2 frames on output (for front & back buffer accordinally) */
-           if ((VPPOut.NumFrameSuggested == 1) || (VPPOut.NumFrameMin == 1))
+           if ((VPPOut.NumFrameSuggested <= 1) || (VPPOut.NumFrameMin <= 1))
            {
                VPPOut.NumFrameSuggested = 2;
                VPPOut.NumFrameMin = 2;
@@ -3273,6 +3337,12 @@ mfxStatus CTranscodingPipeline::AllocFrames()
             if(m_nVPPCompEnable) // WORKAROUND: Remove this if clause after problem with AllocID is fixed in library (mark_alloc++ should be left here)
             {
                 mark_alloc++;
+            }
+
+            if ((DecOut.NumFrameSuggested <= 1) || (DecOut.NumFrameMin <= 1))
+            {
+                DecOut.NumFrameSuggested = 2;
+                DecOut.NumFrameMin = 2;
             }
 
             sts = AllocFrames(&DecOut, true);
@@ -3552,6 +3622,8 @@ mfxStatus CTranscodingPipeline::Init(sInputParams *pParams,
 
     m_encoderFourCC = pParams->EncoderFourCC;
 
+    m_rawInput = pParams->rawInput;
+
     m_bExtMBQP = pParams->bExtMBQP;
     m_bROIasQPMAP = pParams->bROIasQPMAP;
 
@@ -3692,7 +3764,7 @@ mfxStatus CTranscodingPipeline::Init(sInputParams *pParams,
     // opaque memory feature is available starting with API 1.3 and
     // can be used within 1 intra session or joined inter sessions only
     if (m_Version.Major >= 1 && m_Version.Minor >= 3 &&
-        (pParams->eMode == Native || pParams->bIsJoin))
+        (pParams->eMode == Native || pParams->bIsJoin) && !pParams->rawInput)
     {
         m_bUseOpaqueMemory = true;
     }
@@ -3715,12 +3787,15 @@ mfxStatus CTranscodingPipeline::Init(sInputParams *pParams,
     // Initialize pipeline components following downstream direction
     // Pre-init methods fill parameters and create components
 
-    // Decode component initialization
-    sts = DecodePreInit(pParams);
-    if (MFX_ERR_MORE_DATA == sts)
-        return sts;
-    else
-        MSDK_CHECK_STATUS(sts,"DecodePreInit failed");
+    if (!m_rawInput)
+    {
+        // Decode component initialization
+        sts = DecodePreInit(pParams);
+        if (MFX_ERR_MORE_DATA == sts)
+            return sts;
+        else
+            MSDK_CHECK_STATUS(sts,"DecodePreInit failed");
+    }
 
     // VPP component initialization
     sts = VPPPreInit(pParams);

samples/sample_common/include/sample_utils.h
@@ -89,6 +89,8 @@ enum {
 
 #define MFX_CODEC_DUMP MFX_MAKEFOURCC('D','U','M','P')
 #define MFX_CODEC_RGB4 MFX_FOURCC_RGB4
+#define MFX_CODEC_NV12 MFX_FOURCC_NV12
+#define MFX_CODEC_I420 MFX_FOURCC_I420
 
 enum
 {

samples/sample_common/src/sample_utils.cpp
@@ -2310,6 +2310,14 @@ mfxStatus StrFormatToCodecFormatFourCC(msdk_char* strInput, mfxU32 &codecFormat)
         {
             codecFormat = MFX_CODEC_RGB4;
         }
+        else if ((0 == msdk_strcmp(strInput, MSDK_STRING("nv12"))))
+        {
+            codecFormat = MFX_CODEC_NV12;
+        }
+        else if ((0 == msdk_strcmp(strInput, MSDK_STRING("i420"))))
+        {
+            codecFormat = MFX_CODEC_I420;
+        }
         else
             sts = MFX_ERR_UNSUPPORTED;
     }

samples/sample_multi_transcode/include/pipeline_transcode.h
@@ -359,6 +359,8 @@ namespace TranscodingSample
 
         EPresetModes PresetMode;
         bool shouldPrintPresets;
+
+        bool rawInput;
     };
 
     struct sInputParams: public __sInputParams
@@ -724,6 +726,7 @@ namespace TranscodingSample
         mfxStatus InitPluginMfxParams(sInputParams *pInParams);
         mfxStatus InitPreEncMfxParams(sInputParams *pInParams);
 
+        virtual mfxU32 FileFourCC2EncFourCC(mfxU32 fcc);
         void FillFrameInfoForEncoding(mfxFrameInfo& info, sInputParams *pInParams);
 
         mfxStatus AllocAndInitVppDoNotUse(sInputParams *pInParams);
@@ -865,6 +868,8 @@ namespace TranscodingSample
         bool isHEVCSW;
 
         bool m_bInsertIDR;
+
+        bool m_rawInput;
 
         std::unique_ptr<ExtendedBSStore>        m_pBSStore;
 

samples/sample_multi_transcode/src/sample_multi_transcode.cpp
@@ -144,6 +144,7 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
+    ForceImplForSession(0);
     if (m_eDevType == MFX_HANDLE_D3D9_DEVICE_MANAGER)
     {
         m_pAllocParam.reset(new D3DAllocatorParams);
@@ -153,12 +154,14 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
         if (m_InputParamsArray[m_InputParamsArray.size() -1].eModeExt == VppCompOnly)
         {
             /* Rendering case */
-            sts = m_hwdev->Init(NULL, 1, MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D9) );
+            sts = m_hwdev->Init(NULL, 1,
+                MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D9 | MFX_IMPL_BASETYPE(m_InputParamsArray[0].libType)) );
             m_InputParamsArray[m_InputParamsArray.size() -1].m_hwdev = m_hwdev.get();
         }
         else /* NO RENDERING*/
         {
-            sts = m_hwdev->Init(NULL, 0, MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D9) );
+            sts = m_hwdev->Init(NULL, 0,
+                MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D9 | MFX_IMPL_BASETYPE(m_InputParamsArray[0].libType)) );
         }
         MSDK_CHECK_STATUS(sts, "m_hwdev->Init failed");
         sts = m_hwdev->GetHandle(MFX_HANDLE_D3D9_DEVICE_MANAGER, (mfxHDL*)&hdl);
@@ -178,12 +181,14 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
         if (m_InputParamsArray[m_InputParamsArray.size() -1].eModeExt == VppCompOnly)
         {
             /* Rendering case */
-            sts = m_hwdev->Init(NULL, 1, MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D11) );
+            sts = m_hwdev->Init(NULL, 1,
+                MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D11 | MFX_IMPL_BASETYPE(m_InputParamsArray[0].libType)) );
             m_InputParamsArray[m_InputParamsArray.size() -1].m_hwdev = m_hwdev.get();
         }
         else /* NO RENDERING*/
         {
-            sts = m_hwdev->Init(NULL, 0, MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D11) );
+            sts = m_hwdev->Init(NULL, 0,
+                MSDKAdapter::GetNumber(0,MFX_IMPL_VIA_D3D11 | MFX_IMPL_BASETYPE(m_InputParamsArray[0].libType)) );
         }
         MSDK_CHECK_STATUS(sts, "m_hwdev->Init failed");
         sts = m_hwdev->GetHandle(MFX_HANDLE_D3D11_DEVICE, (mfxHDL*)&hdl);
@@ -331,9 +336,11 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
         {
             reader.reset(new CIVFFrameReader());
         }
-        else if (m_InputParamsArray[i].DecodeId == MFX_CODEC_RGB4)
+        else if (m_InputParamsArray[i].DecodeId == MFX_CODEC_RGB4 ||
+                 m_InputParamsArray[i].DecodeId == MFX_CODEC_I420 ||
+                 m_InputParamsArray[i].DecodeId == MFX_CODEC_NV12)
         {
-            // YUV reader for RGB4 overlay
+            // YUV reader for RGB4 overlay and raw input
             yuvreader.reset(new CSmplYUVReader());
         }
         else
@@ -352,7 +359,7 @@ mfxStatus Launcher::Init(int argc, msdk_char *argv[])
         {
             std::list<msdk_string> input;
             input.push_back(m_InputParamsArray[i].strSrcFile);
-            sts = yuvreader->Init(input, MFX_FOURCC_RGB4);
+            sts = yuvreader->Init(input, m_InputParamsArray[i].DecodeId);
             MSDK_CHECK_STATUS(sts, "m_YUVReader->Init failed");
             sts = m_pExtBSProcArray.back()->SetReader(yuvreader);
             MSDK_CHECK_STATUS(sts, "m_pExtBSProcArray.back()->SetReader failed");

samples/sample_multi_transcode/src/transcode_utils.cpp
@@ -131,6 +131,8 @@ void TranscodingSample::PrintHelp()
     msdk_printf(MSDK_STRING("Pipeline description (general options):\n"));
     msdk_printf(MSDK_STRING("  -i::h265|h264|mpeg2|vc1|mvc|jpeg|vp9|av1 <file-name>\n"));
     msdk_printf(MSDK_STRING("                 Set input file and decoder type\n"));
+    msdk_printf(MSDK_STRING("  -i::i420|nv12 <file-name>\n"));
+    msdk_printf(MSDK_STRING("                 Set raw input file and color format\n"));
     msdk_printf(MSDK_STRING("  -i::rgb4_frame Set input rgb4 file for compositon. File should contain just one single frame (-vpp_comp_src_h and -vpp_comp_src_w should be specified as well).\n"));
     msdk_printf(MSDK_STRING("  -o::h265|h264|mpeg2|mvc|jpeg|raw <file-name>\n"));
     msdk_printf(MSDK_STRING("                Set output file and encoder type\n"));
@@ -2633,10 +2635,22 @@ mfxStatus CmdProcessor::VerifyAndCorrectInputParams(TranscodingSample::sInputPar
        MFX_CODEC_VP9 != InputParams.DecodeId &&
        MFX_CODEC_AV1 != InputParams.DecodeId &&
        MFX_CODEC_RGB4 != InputParams.DecodeId &&
+       MFX_CODEC_NV12 != InputParams.DecodeId &&
+       MFX_CODEC_I420 != InputParams.DecodeId &&
        InputParams.eMode != Source)
     {
         PrintError(MSDK_STRING("Unknown decoder\n"));
         return MFX_ERR_UNSUPPORTED;
+    }
+
+    if (MFX_CODEC_I420 == InputParams.DecodeId ||
+        MFX_CODEC_NV12 == InputParams.DecodeId)
+    {
+        InputParams.rawInput = true;
+    }
+    else
+    {
+        InputParams.rawInput = false;
     }
 
     if (MFX_CODEC_RGB4 == InputParams.DecodeId &&

commit 9f934f2b816b887f66c89dee07558984ee919407

smt: fix fps overriding
Fixes: 90a079d ("[sample_multi_transcode] Add support for encoding I420 and NV12 streams")

samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -2315,11 +2315,6 @@ void CTranscodingPipeline::FillFrameInfoForEncoding(mfxFrameInfo& info, sInputPa
     {
         ConvertFrameRate(pInParams->dVPPOutFramerate, &info.FrameRateExtN, &info.FrameRateExtD);
     }
-    else
-    {
-        info.FrameRateExtN = 30;
-        info.FrameRateExtD = 1;
-    }
 }
 
 mfxStatus CTranscodingPipeline::InitEncMfxParams(sInputParams *pInParams)