ac61e45d26bb479a802a633ce12bf0511f74fdae
Nacho Garcia <nacho.garcia@gmail.com>
2020-08-20 19:10:27 +0200

    Use lowercase for windows.h
    To prevent errors when cross-compiling
    from Linux to Windows.
    windows.h is all lowercase in mingw.

--- api/mfx_dispatch/windows/src/main.cpp
+++ api/mfx_dispatch/windows/src/main.cpp
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <Windows.h>
+#include <windows.h>
 #include <stringapiset.h>
 
 #include <new>
