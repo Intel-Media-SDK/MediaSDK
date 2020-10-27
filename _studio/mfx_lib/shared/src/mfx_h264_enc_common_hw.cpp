763ba534abd464d328ea29145b13117fa333a4a8
possosse <sebastian.possos@intel.com>
2020-10-26 18:57:45 -0700

    MCTF: Disable MCTF in AVC pipeline
    Issue: MDP-64761
    Test:  Manual testing

--- _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -1407,6 +1407,8 @@ bool MfxHwH264Encode::IsMctfSupported(
         (video.mfx.FrameInfo.BitDepthLuma == 0 || video.mfx.FrameInfo.BitDepthLuma == 8) &&
         (video.mfx.GopRefDist == 8) &&
         !video.mfx.EncodedOrder);
+#else
+    video;
 #endif
     return isSupported;
 }
