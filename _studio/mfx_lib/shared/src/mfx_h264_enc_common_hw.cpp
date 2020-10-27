0114ac8f98c85ea01c15ec37d779e97591490720
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2020-10-27 06:40:02 -0700

    clang-11: fix build
    Fix: #2425
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -1393,6 +1393,7 @@ bool MfxHwH264Encode::IsCmNeededForSCD(
 bool MfxHwH264Encode::IsMctfSupported(
     MfxVideoParam const & video)
 {
+    (void)video;
     bool
         isSupported = false;
 #if defined(MXF_ENABLE_MCTF_IN_AVC)
@@ -1407,8 +1408,6 @@ bool MfxHwH264Encode::IsMctfSupported(
         (video.mfx.FrameInfo.BitDepthLuma == 0 || video.mfx.FrameInfo.BitDepthLuma == 8) &&
         (video.mfx.GopRefDist == 8) &&
         !video.mfx.EncodedOrder);
-#else
-    video;
 #endif
     return isSupported;
 }
