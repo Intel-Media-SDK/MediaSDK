93ef49e635aaa94cd150cdf7b7a7f76701817980
Dmitry Sorokin <dmitry.sorokin@intel.com>
2021-04-30 16:42:35 +0300

    Update entrypoint.sh

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -5,25 +5,48 @@ time=$(date)
 
 set -ex
 
+# CFLAGS="-O2 -Wformat -Wformat-security -Wall -Werror -D_FORTIFY_SOURCE=2 -fstack-protector-strong"
+
+# echo "Building & installing libva"
+# cd libva
+# ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
+# make -j$(nproc)
+# sudo make install
+# make clean
+# cd ..
+
+# echo "Building oneVPL"
+
+# cd libmfxgen
+# rm -rf build
+# mkdir build && cd build
+# cmake -DAPI=2.3 -G Ninja -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_FLAGS_RELEASE="$CFLAGS" -DCMAKE_CXX_FLAGS_RELEASE="$CFLAGS" ..
+# ninja
+# sudo ninja install
+# cd ..
+# rm -rf build
+
+# echo "::set-output name=status::ok"
+
+
+
 CFLAGS="-O2 -Wformat -Wformat-security -Wall -Werror -D_FORTIFY_SOURCE=2 -fstack-protector-strong"
 
-echo "Building & installing libva"
+
+cmake --version
+$CC --version
+$CXX --version
+
 cd libva
 ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
 make -j$(nproc)
-sudo make install
-make clean
-cd ..
+make install
 
-echo "Building oneVPL"
+cd ../
 
 cd libmfxgen
-rm -rf build
 mkdir build && cd build
-cmake -DAPI=2.3 -G Ninja -DCMAKE_BUILD_TYPE=$1 -DCMAKE_C_FLAGS_RELEASE="$CFLAGS" -DCMAKE_CXX_FLAGS_RELEASE="$CFLAGS" ..
-ninja
-sudo ninja install
-cd ..
-rm -rf build
-
-echo "::set-output name=status::ok"
+cmake -DAPI=latest -DBUILD_ALL=ON -DENABLE_ALL=ON -DENABLE_ITT=OFF -DCMAKE_C_FLAGS_RELEASE="$CFLAGS" -DCMAKE_CXX_FLAGS_RELEASE="$CFLAGS" ..
+make -j$(nproc)
+make test
+make install
