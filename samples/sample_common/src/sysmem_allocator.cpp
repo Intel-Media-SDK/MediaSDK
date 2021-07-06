618db71e011652c44a93ed9a927423a328898f54
Sofya Balandina <sofya.balandina@intel.com>
2021-07-06 13:22:43 +0300

    [samples]: Fixed RGBP R/B channel swap issue
    Issue: None
    Test: Manually

--- samples/sample_common/src/sysmem_allocator.cpp
+++ samples/sample_common/src/sysmem_allocator.cpp
@@ -151,8 +151,8 @@ mfxStatus SysMemFrameAllocator::LockFrame(mfxMemId mid, mfxFrameData *ptr)
         break;
 #if !(defined(_WIN32) || defined(_WIN64))
     case MFX_FOURCC_RGBP:
-        ptr->G = ptr->B + Width2 * Height2;
-        ptr->R = ptr->B + Width2 * Height2 * 2;
+        ptr->G = ptr->R + Width2 * Height2;
+        ptr->B = ptr->G + Width2 * Height2;
         ptr->PitchHigh = (mfxU16)((MSDK_ALIGN32(fs->info.Width)) / (1 << 16));
         ptr->PitchLow = (mfxU16)((MSDK_ALIGN32(fs->info.Width)) % (1 << 16));
         break;
