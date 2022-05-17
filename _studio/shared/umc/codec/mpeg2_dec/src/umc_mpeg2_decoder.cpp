008e785fdfc7a95bb0ab6e49e4112ec6309af6b9
Shawn Li <shawn.li@intel.com>
2022-05-17 10:12:04 +0800

    Check NULL pointer to fix segmen fault
    add null check to fix segmentation fault

--- _studio/shared/umc/codec/mpeg2_dec/src/umc_mpeg2_decoder.cpp
+++ _studio/shared/umc/codec/mpeg2_dec/src/umc_mpeg2_decoder.cpp
@@ -844,6 +844,11 @@ namespace UMC_MPEG2_DECODER
     bool MPEG2Decoder::IsFieldOfCurrentFrame() const
     {
         const auto firstFrameSlice = m_currFrame->GetAU(m_currFrame->fieldIndex)->GetSlice(0);
+        if (!firstFrameSlice)
+        {
+            throw mpeg2_exception(UMC::UMC_ERR_NULL_PTR);
+        }
+
         const auto picHdr = firstFrameSlice->GetPicHeader();
         const auto picExtHdr = firstFrameSlice->GetPicExtHeader();
         const auto newPicHdr = *m_currHeaders.picHdr.get();
